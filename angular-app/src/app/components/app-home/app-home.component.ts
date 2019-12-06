fea708066b81a1d34fc2533a11ecd731c329ea2a
David Ocampo <david.ocampo@bytepushers.software>
2019-12-06 13:47:00 -0600

    Fixed scroll for header.

--- angular-app/src/app/components/app-home/app-home.component.ts
+++ angular-app/src/app/components/app-home/app-home.component.ts
@@ -16,15 +16,4 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
-  public navScroll() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-      const scrollingNav = document.getElementById("topnav");
-      scrollingNav.classList.add("topnav--scrolling");
-      console.log('yay');
-    } else {
-      const scrollingNav = document.getElementById("topnav");
-      scrollingNav.classList.remove("topnav--scrolling");
-      console.log("nay");
-    }
-  }
 }
