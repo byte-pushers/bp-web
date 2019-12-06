0489e403f67cd63d94881cd098215d55f9a3975d
David Ocampo <david.ocampo@bytepushers.software>
2019-12-06 12:53:57 -0600

    Completed scroll for caret.

--- angular-app/src/app/components/app-home/app-home.component.ts
+++ angular-app/src/app/components/app-home/app-home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
+
 @Component({
   selector: 'app-home',
   templateUrl: './app-home.component.html',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  public navScroll() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      const scrollingNav = document.getElementById("topnav");
+      scrollingNav.classList.add("topnav--scrolling");
+      console.log('yay');
+    } else {
+      const scrollingNav = document.getElementById("topnav");
+      scrollingNav.classList.remove("topnav--scrolling");
+      console.log("nay");
+    }
+  }
 }
