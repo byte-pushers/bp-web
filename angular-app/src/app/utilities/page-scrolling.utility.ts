97fd61ada4f90588097e6d81b2cb35acac055d7b
Surya Jamalpuri <SJamalpuri@ad.prolifics.com>
2019-12-10 19:54:33 +0530

    removed extra -  as per comments

--- angular-app/src/app/utilities/page-scrolling.utility.ts
+++ angular-app/src/app/utilities/page-scrolling.utility.ts
@@ -10,11 +10,11 @@ export class PageScrollingUtility {
 /*  public navScroll() {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
       const scrollingNav =  document.getElementById("topnav");
-      scrollingNav.classList.add("topnav--scrolling");
+      scrollingNav.classList.add("topnav-scrolling");
       console.log('yay');
     } else {
       const scrollingNav =  document.getElementById("topnav");
-      scrollingNav.classList.remove("topnav--scrolling");
+      scrollingNav.classList.remove("topnav-scrolling");
       console.log("nay");
     }
   }*/
