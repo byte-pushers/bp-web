d93be5a003ac356fe3062b2827986a8c8480a056
David Ocampo <david.ocampo@bytepushers.software>
2019-12-11 16:37:20 -0600

    Completed work on mobile nav across all devices as well as responsiveness of them.

--- angular-app/src/app/shared/components/app-header.component/app-header.component.ts
+++ angular-app/src/app/shared/components/app-header.component/app-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,23 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app-header.component.css']
 })
 export class AppHeaderComponent implements OnInit {
-
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
-    window.onscroll = function() {navScroll()};
+    window.onscroll = function () {
+      navScroll()
+    };
 
     function navScroll() {
+      const mobileNav = document.getElementById('topnav');
       if (document.body.scrollTop > 570 || document.documentElement.scrollTop > 570) {
-        const scrollingNav =  document.getElementById("topnav");
+        const scrollingNav = document.getElementById("topnav");
         scrollingNav.classList.add("topnav-scrolling");
+        mobileNav.classList.remove("expanded");
 
       } else {
-        const scrollingNav =  document.getElementById("topnav");
+        const scrollingNav = document.getElementById("topnav");
         scrollingNav.classList.remove("topnav-scrolling");
 
       }
     }
+
   }
+
+  public openCloseMobileNav() {
+    const mobileNav = document.getElementById('topnav');
+
+    if (mobileNav.classList.contains('expanded')) {
+      mobileNav.classList.remove("expanded");
+    } else {
+      mobileNav.classList.add("expanded");
+    }
+  }
+
 
 }
