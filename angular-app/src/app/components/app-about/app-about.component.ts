964d361a8a75daceba02bb20dfdbcbb4dd426839
Tonté Pouncil <pouncilt.developer@gmail.com>
2020-01-02 13:54:31 -0600

    WIP - Use jQuery to register on click event handlers for Next and Previous slide buttons.

--- angular-app/src/app/components/app-about/app-about.component.ts
+++ angular-app/src/app/components/app-about/app-about.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { CarouselModule } from 'ngx-bootstrap/carousel';
+import {Component, OnInit} from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-about',
@@ -9,12 +9,34 @@ import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit() {
-  }
-public reverseAnimation () {
-  const animation = document.getElementsByClassName('active');
+    console.log("ngOnInit()");
 
-}
+
+    $(document).ready(function(e) {
+      const $prevButton = $(".left.carousel-control.carousel-control-prev");
+      const $nextButton = $(".right.carousel-control.carousel-control-next");
+
+      $prevButton.click((event) => {
+        console.log("previous button clicked.");
+        $("slide.item.carousel-item").removeClass("left-right");
+        $("slide.item.carousel-item").addClass("right-left");
+      });
+
+      $nextButton.click((event) => {
+        console.log("next button clicked.");
+        $("slide.item.carousel-item").removeClass("right-left");
+        $("slide.item.carousel-item").addClass("left-right");
+      });
+    });
+
+  }
+
+  public reverseAnimation() {
+    const animation = document.getElementsByClassName('active');
+  }
 }
