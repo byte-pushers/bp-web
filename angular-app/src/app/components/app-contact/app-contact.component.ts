2a5776313442840f3f19da59cddb07c86270cc35
ocampd1 <david.ocampo@bytepushers.software>
2020-01-30 13:04:55 -0600

    Removed commented out code.

--- angular-app/src/app/components/app-contact/app-contact.component.ts
+++ angular-app/src/app/components/app-contact/app-contact.component.ts
@@ -230,10 +230,4 @@ export class ContactComponent implements OnInit {
     }
   }
 
-  /*
-    compareFn(c1: any, c2: any): boolean {
-      return c1 && c2 ? c1.id === c2.id : c1 === c2;
-    }
-  */
-
 }
