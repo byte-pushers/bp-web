ca47ec4c7d9abaa76b171cbeff6205d93c8f234c
ocampd1 <david.ocampo@bytepushers.software>
2020-09-15 09:07:23 -0500

    Completed work on temporary info page.

---
+++ angular-app/src/app/components/app-info/app-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-info',
+  templateUrl: './app-info.component.html',
+  styleUrls: ['./app-info.component.css']
+})
+export class InfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
