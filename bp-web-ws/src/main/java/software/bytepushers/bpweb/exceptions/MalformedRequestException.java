60097fbc860c45cb36ee765050e7c78b03208f4d
Aman Abrol <amanjotabroljava@gmail.com>
2020-09-25 20:53:19 -0700

    Trying to deploy build bp web on travis

---
+++ bp-web-ws/src/main/java/software/bytepushers/bpweb/exceptions/MalformedRequestException.java
@@ -0,0 +1,5 @@
+package software.bytepushers.bpweb.exceptions;
+
+public class MalformedRequestException extends ApplicationMessageException {
+    public MalformedRequestException(String... msgs) { super(msgs); }
+}
---
+++ bp-web-ws/src/main/java/software/bytepushers/bpweb/model/dto/PersonDto.java
@@ -0,0 +1,33 @@
+package software.bytepushers.bpweb.model.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+/**
+ * The person dto model.
+ */
+@Getter
+@Setter
+public class PersonDto extends AbstractDto {
+
+    @NotEmpty(message = "{person.firstname.required}")
+    private String firstName;
+
+    @NotEmpty(message = "{person.lastname.required}")
+    private String lastName;
+
+    @NotEmpty(message = "{person.email.required}")
+    @Email(message = "{person.email.invalid}")
+    private String email;
+
+    @NotEmpty(message = "{person.phone.required}")
+    private String phone;
+
+    @NotNull(message = "{person.address.details.required}")
+    private AddressDto address;
+
+}
