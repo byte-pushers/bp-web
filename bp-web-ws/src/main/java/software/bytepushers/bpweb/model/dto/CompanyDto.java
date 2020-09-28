3f21d97542552ad555202a64a9c938a192b59761
Aman Abrol <amanjotabroljava@gmail.com>
2020-09-27 20:56:50 -0700

    Validation improvements

--- bp-web-ws/src/main/java/software/bytepushers/bpweb/model/dto/CompanyDto.java
+++ bp-web-ws/src/main/java/software/bytepushers/bpweb/model/dto/CompanyDto.java
@@ -14,24 +14,24 @@ import javax.validation.constraints.NotNull;
 @Setter
 public class CompanyDto extends AbstractDto {
 
-    @NotEmpty(groups = {CreateRequest.class}, message = "{company.name.required}")
+    @NotEmpty(message = "{company.name.required}")
     private String name;
 
-    @NotEmpty(groups = {CreateRequest.class}, message = "{company.type.required}")
+    @NotEmpty(message = "{company.type.required}")
     private String type;
 
-    @NotEmpty(groups = {CreateRequest.class}, message = "{company.url.required}")
+    @NotEmpty(message = "{company.url.required}")
     private String url;
 
-    @NotEmpty(groups = {CreateRequest.class}, message = "{company.description.required}")
+    @NotEmpty(message = "{company.description.required}")
     private String description;
 
     @Valid
-    @NotNull(groups = {CreateRequest.class}, message = "{company.budget.required}")
+    @NotNull(message = "{company.budget.required}")
     private BudgetDto budget;
 
     @Valid
-    @NotNull(groups = {CreateRequest.class}, message = "{company.timeline.required}")
+    @NotNull(message = "{company.timeline.required}")
     private TargetTimeLineDto timeline;
 
 }
