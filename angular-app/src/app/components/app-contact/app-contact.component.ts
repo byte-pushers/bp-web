8d5a3e9a08a051293b8aed22224ee446e831c995
Tonté Pouncil <pouncilt.developer@gmail.com>
2020-10-09 12:42:16 -0500

    WIP - Got one form element working via Template Form.

--- angular-app/src/app/components/app-contact/app-contact.component.ts
+++ angular-app/src/app/components/app-contact/app-contact.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {NgForm} from "@angular/forms";
 import {QuoteService} from "../../shared/services/quote.service";
-import {NgxBootstrapSliderService} from "ngx-bootstrap-slider";
 import {Quote} from "../../shared/models/quote";
+import {QuoteModel} from "../../shared/models/quote.model";
 
 
 @Component({
@@ -11,7 +11,9 @@ import {Quote} from "../../shared/models/quote";
   styleUrls: ['./app-contact.component.css']
 })
 export class ContactComponent implements OnInit {
-  public quote: Quote = null;
+  // @ts-ignore
+  @ViewChild('quoteForm') quoteForm: any;
+  public quote: Quote = new QuoteModel(QuoteModel.DEFAULT_CONFIG);
   isSubmitted = false;
   states: any = ['Alabama',
     'Alaska',
@@ -69,8 +71,7 @@ export class ContactComponent implements OnInit {
   budgets: any  = ['Slide for budget amount'];
   timelines: any = ['1-3 Months', '3-6 Months', '6+ Months'];
 
-
-  constructor(public quoteService: QuoteService) {
+  constructor(private quoteService: QuoteService) {
 
   }
 
@@ -78,129 +79,28 @@ export class ContactComponent implements OnInit {
 
   }
 
-  public createQuote(newQuote){
-    newQuote = this.quoteForm.value;
-    if (newQuote !== null && newQuote !== undefined){
-      this.quoteService.createQuote(newQuote).subscribe(newlyCreatedQuote => {
-        // TODO should have a new object with IDs populated through out the object graph.
-        console.log('newly created quote: ' + newlyCreatedQuote, newlyCreatedQuote);
-        console.log(newQuote.firstName)
-        alert('Sucessfully submitted quote');
-
-      }, error => {
-        // TODO should display error message at top of quote page.
-        console.log('error: ' + error, error);
-      });
-      console.log(this.quoteForm);
-    }
-  }
-
-
-
-
-
-
-  changeState(e) {
-    this.states.setValue(e.target.value, {
-      onlySelf: true
-    });
-  }
-
-
-
-
-  changeProjectType(e) {
-    this.projects.setValue(e.target.value, {
-      onlySelf: true
-    });
-  }
-
-   changeBusinessType(e) {
-    this.businessTypes.setValue(e.target.value, {
-      onlySelf: true
-    });
-  }
-
-
-  changeBudget(e) {
-    this.budgets.setValue(e.target.value, {
-      onlySelf: true
-    });
-
-  }
-
-  get timeline() {
-    return this.quoteForm.get(['company', 'timeline']);
-  }
-
-  changeTimeline(e) {
-    this.timelines.setValue(e.target.value, {
-      onlySelf: true
-    })
-  }
-
   public onSubmit(): boolean {
     this.isSubmitted = true;
     if (!this.quoteForm.valid) {
       return false;
     } else {
-      this.createQuote(this.quote);
+      this.saveQuote();
     }
   }
 
-  /*public changeBudgetOnScrollSmall() {
-    const newSmall = 'Small'.concat((': ') + this.value[0] + (' - $30,000' ));
-    this.budgets.splice(0, 1, newSmall);
-
+  public reset(form: NgForm)  {
+    form.resetForm();
   }
 
-
-  public changeBudgetOnScrollMedium() {
-    const newMed = 'Medium'.concat((': ') + this.value[0] + (' - $60,000' ));
-    this.budgets.splice(0, 1, newMed);
+  private saveQuote(){
+    if (this.quote !== null && this.quote !== undefined){
+      this.quoteService.createQuote(this.quote).subscribe(newlyCreatedQuote => {
+        // TODO should have a new object with IDs populated through out the object graph.
+        console.log('newly created quote: ' + newlyCreatedQuote, newlyCreatedQuote);
+      }, error => {
+        // TODO should display error message at top of quote page.
+        console.log('error: ' + error, error);
+      })
+    }
   }
-
-  public changeBudgetOnScrollLarge() {
-    const newLarge = 'Large'.concat((': ') + this.value[0] + (' - $100,000' ));
-    this.budgets.splice(0, 1, newLarge);
-  }
-
-  public changeTimelineOnScrollSmall() {
-    this.timeline.setValue(this.timelines[0]);
-  }
-
-  public changeTimelineOnScrollMedium() {
-    this.timeline.setValue(this.timelines[1]);
-  }
-
-  public changeTimelineOnScrollLarge() {
-    this.timeline.setValue(this.timelines[2]);
-  }*/
-
-  /*  public change() {
-      this.changeBudgetDropdown();
-      this.changeTimelineDropdown();
-    }*/
-
-  /*  public changeBudgetDropdown() {
-      if (this.value[0] <= 30000) {
-        this.changeBudgetOnScrollSmall();
-      } else if (this.value[0] > 30 && this.value[1] < 60000) {
-        this.changeBudgetOnScrollMedium();
-      } else if (this.value[1] > 60000) {
-        this.changeBudgetOnScrollLarge();
-      }
-    }*/
-
-  /*  public changeTimelineDropdown() {
-      if (this.value[0] <= 30000) {
-        this.changeTimelineOnScrollSmall();
-      } else if (this.value[0] > 30000 && this.value[1] < 60000) {
-        this.changeTimelineOnScrollMedium();
-      } else if (this.value[1] > 60000) {
-        this.changeTimelineOnScrollLarge();
-      }
-    }*/
-
-
 }
