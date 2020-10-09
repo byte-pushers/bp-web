6f019be6dc8305b1fb1728ad6385ed6bce2fe1c0
ocampd1 <david.ocampo@bytepushers.software>
2020-10-09 11:38:53 -0500

    WIP adding template to form, implemented patterns.

--- angular-app/src/app/components/app-contact/app-contact.component.ts
+++ angular-app/src/app/components/app-contact/app-contact.component.ts
@@ -1,10 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
-import {phoneNumberValidator} from "../../services/phone-validator.service";
-import {emailValidator} from "../../services/email-validator.service";
+import {NgForm} from "@angular/forms";
 import {QuoteService} from "../../shared/services/quote.service";
 import {NgxBootstrapSliderService} from "ngx-bootstrap-slider";
-import {concat} from "rxjs";
 import {Quote} from "../../shared/models/quote";
 
 
@@ -16,40 +13,6 @@ import {Quote} from "../../shared/models/quote";
 export class ContactComponent implements OnInit {
   public quote: Quote = null;
   isSubmitted = false;
-  quoteForm = new FormGroup({
-    contact: new FormGroup({
-      firstName: new FormControl('', [
-        Validators.required, Validators.min(2), Validators.pattern(/^[a-zA-Z]*$/)]),
-      lastName: new FormControl('', [
-        Validators.required, Validators.min(2), Validators.pattern(/^[a-zA-Z]*$/)]),
-      email: new FormControl('', [Validators.required, emailValidator]),
-      number: new FormControl('', [Validators.required, phoneNumberValidator]),
-      address: new FormGroup({
-        city: new FormControl('', [
-          Validators.required, Validators.min(2), Validators.pattern(/^[a-z\sA-Z]*$/)]),
-        state: new FormControl('', Validators.required),
-        zip: new FormControl(''),
-        street: new FormControl(''),
-        country: new FormControl('')
-      }),
-    }),
-    company: new FormGroup({
-      name: new FormControl('', [
-        Validators.required, Validators.min(2), Validators.pattern(/^[a-zA-Z]*$/)]),
-      type: new FormControl(''),
-      budget: new FormGroup({
-        min: new FormControl(''),
-        max: new FormControl('')
-      }),
-      url: new FormControl('', [Validators.min(2), Validators.pattern(/^((ftp|http|https):\/\/)?www\.([A-z]+)\.([A-z]{2,})/)]),
-      timeline: new FormGroup({
-        min: new FormControl(''),
-        max: new FormControl('')
-      }),
-      desc: new FormControl(''),
-      est: new FormControl('')
-    })
-  });
   states: any = ['Alabama',
     'Alaska',
     'Arizona',
@@ -132,33 +95,10 @@ export class ContactComponent implements OnInit {
     }
   }
 
-  get firstName() {
-    return this.quoteForm.get(['contact', 'firstName']);
-  }
 
-  get lastName() {
-    return this.quoteForm.get(['contact', 'lastName']);
-  }
 
-  get number() {
-    return this.quoteForm.get(['contact', 'number']);
-  }
 
-  get email() {
-    return this.quoteForm.get(['contact', 'email']);
-  }
 
-  get venture() {
-    return this.quoteForm.get(['company', 'venture']);
-  }
-
-  get city() {
-    return this.quoteForm.get(['contact', 'address', 'city']);
-  }
-
-  get state() {
-    return this.quoteForm.get(['contact', 'address', 'state']);
-  }
 
   changeState(e) {
     this.states.setValue(e.target.value, {
@@ -167,17 +107,7 @@ export class ContactComponent implements OnInit {
   }
 
 
-  get name() {
-    return this.quoteForm.get(['company', 'name']);
-  }
 
-  get url() {
-    return this.quoteForm.get(['company', 'url']);
-  }
-
-  get type() {
-    return this.quoteForm.get(['company', 'type']);
-  }
 
   changeProjectType(e) {
     this.projects.setValue(e.target.value, {
@@ -185,19 +115,12 @@ export class ContactComponent implements OnInit {
     });
   }
 
-  get est() {
-    return this.quoteForm.get(['company', 'type']);
-  }
-
-  changeBusinessType(e) {
+   changeBusinessType(e) {
     this.businessTypes.setValue(e.target.value, {
       onlySelf: true
     });
   }
 
-  get budget() {
-    return this.quoteForm.get(['company', 'est']);
-  }
 
   changeBudget(e) {
     this.budgets.setValue(e.target.value, {
@@ -214,10 +137,6 @@ export class ContactComponent implements OnInit {
     this.timelines.setValue(e.target.value, {
       onlySelf: true
     })
-  }
-
-  get desc() {
-    return this.quoteForm.get(['company', 'desc']);
   }
 
   public onSubmit(): boolean {
