93f4071f8f1723cae7b3d6cc7b30285aa1d1888c
Aman Abrol <amanjotabroljava@gmail.com>
2020-11-07 10:35:13 -0700

    Reverted changes which have been made reflected by Find/Replace action in development tool

--- bp-web-ws/src/test/java/software/bytepushers/bpweb/controller/AbstractControllerTest.java
+++ bp-web-ws/src/test/java/software/bytepushers/bpweb/controller/AbstractControllerTest.java
@@ -4,14 +4,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.retrieveTimeframe.context.SpringBootTest;
-import org.springframework.retrieveTimeframe.context.junit4.SpringRunner;
-import org.springframework.retrieveTimeframe.web.servlet.MockMvc;
-import org.springframework.retrieveTimeframe.web.servlet.setup.MockMvcBuilders;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 /**
- * Abstract controller retrieveTimeframe class for controller setup
+ * Abstract controller test class for controller setup
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
--- bp-web-ws/src/test/java/software/bytepushers/bpweb/repository/QuoteRepositoryTest.java
+++ bp-web-ws/src/test/java/software/bytepushers/bpweb/repository/QuoteRepositoryTest.java
@@ -3,13 +3,13 @@ package software.bytepushers.bpweb.repository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.retrieveTimeframe.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.retrieveTimeframe.context.junit4.SpringRunner;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import software.bytepushers.bpweb.model.entity.Quote;
 import software.bytepushers.bpweb.utils.ModelUtils;
 
 /**
- * The retrieveTimeframe case implementation for the Quote repository layer
+ * The test case implementation for the Quote repository layer
  */
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -19,7 +19,7 @@ public class QuoteRepositoryTest {
     private QuoteRepository quoteRepository;
 
     /**
-     * The retrieveTimeframe case implementation is responsible for validating the save operation for the quote.
+     * The test case implementation is responsible for validating the save operation for the quote.
      */
     @Test
     public void testSave() {
