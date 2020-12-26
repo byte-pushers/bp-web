04919ad4f2b46ba60972d1ebdd80f11c374be56e
Aman Abrol <amanjotabroljava@gmail.com>
2020-12-26 10:44:36 -0700

    Updated the URLs for static app

--- bp-web-client/angular-app/src/environments/environment.prod.ts
+++ bp-web-client/angular-app/src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  QUOTE_SERVICE: {
+    API: {
+      HOST: 'https://api.bytepushers.io/Prod/api/v1/quotes',
+      EXT: '.json'
+    }
+  }
 };
