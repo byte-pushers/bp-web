c45606f8f229190ed2400d0adef914a6804dd230
Aman Abrol <amanjotabroljava@gmail.com>
2020-12-26 01:27:30 -0700

    Trying to test the deployment

---
+++ bp-web-ws/src/main/java/software/bytepushers/bpweb/controller/HealthController.java
@@ -0,0 +1,26 @@
+package software.bytepushers.bpweb.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * The rest endpoints for the health endpoint.
+ */
+@Log4j2
+@RestController
+@RequestMapping("/api/v1/health")
+public class HealthController extends AbstractController {
+
+    /**
+     * Health endpoint implementation.
+     *
+     * @return the all quote details.
+     */
+    @GetMapping
+    public ResponseEntity<?> getAll() {
+        return sendOkResponse("Up and Running");
+    }
+}
