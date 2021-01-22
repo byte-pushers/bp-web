1b40455edebf8da6c1498847acfa8f80b66826d7
ocampd1 <david.ocampo@bytepushers.software>
2021-01-22 09:07:17 -0800

    Changed registry from create to quotes.

--- bp-web-ws/src/main/java/software/bytepushers/bpweb/config/CorsConfig.java
+++ bp-web-ws/src/main/java/software/bytepushers/bpweb/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/create")
+                registry.addMapping("/quotes")
                         .allowedOrigins("http://localhost:4200")
                         .allowedHeaders("*")
                         .allowedMethods("*");
