d138ca5526f4c346fa8a088b07b9712014ac9d97
Aman Abrol <amanjotabroljava@gmail.com>
2021-02-05 21:11:41 -0700

    Mentioned specific HTTP methods to allow

--- bp-web-ws/src/main/java/software/bytepushers/bpweb/config/CorsConfig.java
+++ bp-web-ws/src/main/java/software/bytepushers/bpweb/config/CorsConfig.java
@@ -4,14 +4,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import static org.springframework.http.HttpMethod.*;
+
 /**
  * Enable CORS for the all requests
  */
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+    /**
+     * {@inheritDoc}
+     *
+     * @param registry to configure the CORS origin attributes
+     */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**").allowedMethods(GET.name(), HEAD.name(), POST.name(),
+                PUT.name(), DELETE.name(), OPTIONS.name(), HEAD.name());
     }
 }
