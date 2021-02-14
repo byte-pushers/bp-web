40257f6bd3ab35ffaced9d284e358171e425d882
ocampd1 <ocampdesigns@gmail.com>
2021-02-14 14:43:22 -0800

    Completed adjustments as needed for contact page.

--- bp-web-client/angular-app/src/app/services/scroll.service.ts
+++ bp-web-client/angular-app/src/app/services/scroll.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
+import {ContactButtonService} from './contact-button.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ScrollService {
 
-  constructor() { }
+  constructor(public onContact: ContactButtonService) { }
 
   public backToTop() {
     document.body.scrollTop = 0;
