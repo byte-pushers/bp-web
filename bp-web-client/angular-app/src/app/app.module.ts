ea3dfacb93e6cd8db87144a70ce5234e80bbb4bd
ocampd1 <ocampdesigns@gmail.com>
2021-02-18 13:32:04 -0800

    Completed additional changes to branch.

--- bp-web-client/angular-app/src/app/app.module.ts
+++ bp-web-client/angular-app/src/app/app.module.ts
@@ -1,34 +1,34 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
-import {OverlayModule} from '@angular/cdk/overlay';
-import {AppComponent} from './shared/components/app.component/app.component';
-import {AppHeaderComponent} from './shared/components/app-header.component/app-header.component';
-import {AppBodyComponent} from './shared/components/app-body.component/app-body.component';
-import {HomeComponent} from './components/app-home/app-home.component';
-import {ContactComponent} from './components/app-contact/app-contact.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {CarouselModule} from 'ngx-bootstrap/carousel';
-import {NgBootstrapFormValidationModule} from 'ng-bootstrap-form-validation';
-import {FormsModule} from '@angular/forms';
-import {NgxBootstrapSliderModule} from 'ngx-bootstrap-slider';
-import {QuoteService} from './shared/services/quote.service';
-import {AppFooterComponent} from './shared/components/app-footer/app-footer.component';
-import {NgxSpinnerModule} from 'ngx-spinner';
-import {MatCardModule} from '@angular/material/card';
-import {AppAlertOverlayModalComponent} from './shared/components/app-alert-overlay-modal.component/app-alert-overlay-modal.component';
-import {NgxPageScrollCoreModule} from 'ngx-page-scroll-core';
-import {NgxPageScrollModule} from 'ngx-page-scroll';
-import {RouterLinkActive, RouterModule} from '@angular/router';
-import {InfoComponent} from './components/app-info/app-info.component';
-import {AboutComponent} from './components/app-about/app-about.component';
-import {WorkComponent} from './components/app-work/app-work.component';
-import {ServicesComponent} from './components/app-services/app-services.component';
-import {PhoneNumberDirective} from './directives/phone-number.directive';
-import {AppRoutingModule} from './app-routing.module';
-import {AppRoutingService} from './shared/services/app-routing.service';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { AppComponent } from './shared/components/app.component/app.component';
+import { AppHeaderComponent } from './shared/components/app-header.component/app-header.component';
+import { AppBodyComponent } from './shared/components/app-body.component/app-body.component';
+import { HomeComponent } from './components/app-home/app-home.component';
+import { ContactComponent } from './components/app-contact/app-contact.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { NgBootstrapFormValidationModule } from 'ng-bootstrap-form-validation';
+import { FormsModule } from '@angular/forms';
+import { NgxBootstrapSliderModule } from 'ngx-bootstrap-slider';
+import { QuoteService } from './shared/services/quote.service';
+import { AppFooterComponent } from './shared/components/app-footer/app-footer.component';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { MatCardModule } from '@angular/material/card';
+import { AppAlertOverlayModalComponent } from './shared/components/app-alert-overlay-modal.component/app-alert-overlay-modal.component';
+import { NgxPageScrollCoreModule } from 'ngx-page-scroll-core';
+import { NgxPageScrollModule } from 'ngx-page-scroll';
+import { RouterLinkActive } from '@angular/router';
+import { InfoComponent } from './components/app-info/app-info.component';
+import { AboutComponent } from './components/app-about/app-about.component';
+import { WorkComponent } from './components/app-work/app-work.component';
+import { ServicesComponent } from './components/app-services/app-services.component';
+import { PhoneNumberDirective } from './directives/phone-number.directive';
+import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingService } from './shared/services/app-routing.service';
 import { CommonModule } from '@angular/common';
-import {ContactButtonService} from './services/contact-button.service';
+import { ContactButtonService } from './services/contact-button.service';
 
 @NgModule({
   declarations: [
@@ -67,7 +67,6 @@ import {ContactButtonService} from './services/contact-button.service';
     QuoteService,
     {provide: Window, useValue: window},
     ContactButtonService,
-    {provide: Window, useValue: window},
     AppRoutingService
   ],
   entryComponents: [
