d3a40c751aea6dfaf562896ef388645b66f5c7b2
ocampd1 <ocampdesigns@gmail.com>
2021-02-18 16:00:27 -0800

    Updated scroll service.

--- bp-web-client/angular-app/src/app/services/scroll.service.ts
+++ bp-web-client/angular-app/src/app/services/scroll.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import {ContactButtonService} from './contact-button.service';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ScrollService {
 
-  constructor(public onContact: ContactButtonService) { }
+  constructor() { }
 
   public backToTop() {
     document.body.scrollTop = 0;
