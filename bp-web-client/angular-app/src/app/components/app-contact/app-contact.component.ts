35e81480615b93731ecccf845cb7f661fadc904e
Surya Jamalpur <surya.jamalpuri@prolifics.com>
2023-01-19 20:09:24 +0530

    removed unnessesory file

--- bp-web-client/angular-app/src/app/components/app-contact/app-contact.component.ts
+++ bp-web-client/angular-app/src/app/components/app-contact/app-contact.component.ts
@@ -1,59 +1,68 @@
-import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
-import {NgForm} from '@angular/forms';
-import {QuoteService} from '../../shared/services/quote.service';
-import {Quote} from '../../shared/models/quote';
-import {QuoteModel} from '../../shared/models/quote.model';
-import {NgxSpinnerService} from 'ngx-spinner';
-import {AppAlertOverlayModalService} from '../../shared/components/app-alert-overlay-modal.component/app-alert-overlay-modal.service';
-import {ScrollToService} from '../../services/scroll-to.service';
-import {AppAlertOverlayModalComponent} from '../../shared/components/app-alert-overlay-modal.component/app-alert-overlay-modal.component';
-import {ComponentType} from '@angular/cdk/portal/portal';
-import {StateNameService} from '../../services/state-name.service';
-import {ContactButtonService} from '../../services/contact-button.service';
-
+import {
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { QuoteService } from "../../shared/services/quote.service";
+import { Quote } from "../../shared/models/quote";
+import { QuoteModel } from "../../shared/models/quote.model";
+import { NgxSpinnerService } from "ngx-spinner";
+import { AppAlertOverlayModalService } from "../../shared/components/app-alert-overlay-modal.component/app-alert-overlay-modal.service";
+import { ScrollToService } from "../../services/scroll-to.service";
+import { AppAlertOverlayModalComponent } from "../../shared/components/app-alert-overlay-modal.component/app-alert-overlay-modal.component";
+import { ComponentType } from "@angular/cdk/portal";
+import { StateNameService } from "../../services/state-name.service";
+import { ContactButtonService } from "../../services/contact-button.service";
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './app-contact.component.html',
-  styleUrls: ['./app-contact.component.css']
+  selector: "app-contact",
+  templateUrl: "./app-contact.component.html",
+  styleUrls: ["./app-contact.component.css"],
 })
 export class ContactComponent implements OnInit, OnDestroy {
   public errorMessage: string;
-  public errorMessages: [string?] = ['Phone number is invalid.'];
+  public errorMessages: [string?] = ["Phone number is invalid."];
   public showConfirmation = false;
-  public phone: { number: string } = {number: ''};
+  public phone: { number: string } = { number: "" };
 
-  constructor(private quoteService: QuoteService,
-              private spinner: NgxSpinnerService,
-              private appAlertOverlayModalService: AppAlertOverlayModalService,
-              public stateNameService: StateNameService,
-              private scrollToService: ScrollToService,
-              private contactButtonService: ContactButtonService) {
+  constructor(
+    private quoteService: QuoteService,
+    private spinner: NgxSpinnerService,
+    private appAlertOverlayModalService: AppAlertOverlayModalService,
+    public stateNameService: StateNameService,
+    private scrollToService: ScrollToService,
+    private contactButtonService: ContactButtonService
+  ) {}
 
-  }
-
-  @ViewChild('quoteForm') quoteForm: any;
-  @ViewChild('phoneNumber') phoneNumber: any;
+  @ViewChild("quoteForm") quoteForm: any;
+  @ViewChild("phoneNumber") phoneNumber: any;
   public quote: Quote = new QuoteModel(QuoteModel.DEFAULT_CONFIG);
   hidePersonal = false;
   hidePersonalBtn = true;
   hideBusiness = false;
   hideBusinessBtn = true;
   isSubmitted = false;
-  projects: any = ['Business To Business (B2B)', 'Business To Consumer (B2C)', 'Custom App Development', 'Other'];
-  projectPlatforms: any = ['Mobile', 'Desktop', 'Cloud', 'Web', 'Other'];
+  projects: any = [
+    "Business To Business (B2B)",
+    "Business To Consumer (B2C)",
+    "Custom App Development",
+    "Other",
+  ];
+  projectPlatforms: any = ["Mobile", "Desktop", "Cloud", "Web", "Other"];
   value: any = [0, 105000];
-  budgets: any = ['Slide for budget amount'];
-  timelines: any = ['Slide for time frame'];
+  budgets: any = ["Slide for budget amount"];
+  timelines: any = ["Slide for time frame"];
   timeframe: any = [0, 39];
   years = [];
-  foundations: any = ['New Business', 'Existing Business'];
+  foundations: any = ["New Business", "Existing Business"];
 
   ngOnInit() {
     this.years = this.calculateYears(+new Date().getFullYear(), 40);
-    this.years.push('Older than 1980');
+    this.years.push("Older than 1980");
     this.setOnContactView(false);
-
   }
 
   ngOnDestroy() {
@@ -62,7 +71,7 @@ export class ContactComponent implements OnInit, OnDestroy {
   public setOnContactView(setView): void {
     this.contactButtonService.isOnContactView(setView);
   }
-  public calculateYears(yearList: number, yearsSpan: number): any [] {
+  public calculateYears(yearList: number, yearsSpan: number): any[] {
     const yearArray = [];
     yearArray.push(yearList);
     for (let i = 1; i < yearsSpan; i++) {
@@ -70,7 +79,6 @@ export class ContactComponent implements OnInit, OnDestroy {
     }
     return yearArray;
   }
-
 
   public isMobileResolution(): boolean {
     let isMobileResolution = false;
@@ -90,19 +98,19 @@ export class ContactComponent implements OnInit, OnDestroy {
     if (!this.quoteForm.valid) {
       this.isSubmitted = false;
     } else {
-    this.saveQuote();
+      this.saveQuote();
     }
   }
-public backToTop() {
+  public backToTop() {
     this.scrollToService.toTopOfPage();
-}
+  }
 
   public onSubmitBackToTopDesktop() {
-    document.getElementById('successTop').scrollIntoView();
+    document.getElementById("successTop").scrollIntoView();
   }
 
   public onSubmitBackToTopMobile() {
-    document.getElementById('successTop').scrollIntoView();
+    document.getElementById("successTop").scrollIntoView();
   }
 
   public reset(form: NgForm) {
@@ -127,34 +135,41 @@ public backToTop() {
     this.spinner.show();
     if (this.quote !== null && this.quote !== undefined) {
       this.quote.contact.phone.number = this.phoneNumber.control.value;
-      this.quoteService.createQuote(this.quote).subscribe(newlyCreatedQuote => {
-        // TODO should have a new object with IDs populated through out the object graph.
-        console.log('newly created quote: ' + newlyCreatedQuote, newlyCreatedQuote);
-        this.showConfirmation = true;
-        this.spinner.hide();
+      this.quoteService.createQuote(this.quote).subscribe(
+        (newlyCreatedQuote) => {
+          // TODO should have a new object with IDs populated through out the object graph.
+          console.log(
+            "newly created quote: " + newlyCreatedQuote,
+            newlyCreatedQuote
+          );
+          this.showConfirmation = true;
+          this.spinner.hide();
 
-        // TODO: Maybe we don't need this logic.
-        if (this.isMobileResolution()) {
+          // TODO: Maybe we don't need this logic.
+          if (this.isMobileResolution()) {
             this.onSubmitBackToTopMobile();
           } else {
             this.onSubmitBackToTopDesktop();
           }
-      }, error => {
-        // TODO should display error message at top of quote page.
-        console.log('error: ' + error, error);
-        this.errorMessages.push('Account was not created, internal error.');
+        },
+        (error) => {
+          // TODO should display error message at top of quote page.
+          console.log("error: " + error, error);
+          this.errorMessages.push("Account was not created, internal error.");
 
-        if (this.isMobileResolution()) {
-          this.showOverlayModal(this.errorMessages[0]);
+          if (this.isMobileResolution()) {
+            this.showOverlayModal(this.errorMessages[0]);
+          }
+
+          this.spinner.hide();
         }
-
-        this.spinner.hide();
-      });
+      );
     }
   }
 
   public showOverlayModal(message?: string) {
-    const component: ComponentType<AppAlertOverlayModalComponent> = AppAlertOverlayModalComponent;
+    const component: ComponentType<AppAlertOverlayModalComponent> =
+      AppAlertOverlayModalComponent;
     this.appAlertOverlayModalService.setMessage(message);
     this.appAlertOverlayModalService.open(component);
   }
@@ -162,8 +177,12 @@ public backToTop() {
   public changeTimeline() {
     const timeFrameMin = this.timeframe[0];
     const timeFrameMax = this.timeframe[1];
-    const newTimeline = ''.concat(timeFrameMin + (' mo - ') + timeFrameMax + (' mo'));
-    const newTimelineTop = ''.concat(timeFrameMin + (' mo - ') + timeFrameMax + (' mo or longer'));
+    const newTimeline = "".concat(
+      timeFrameMin + " mo - " + timeFrameMax + " mo"
+    );
+    const newTimelineTop = "".concat(
+      timeFrameMin + " mo - " + timeFrameMax + " mo or longer"
+    );
 
     if (timeFrameMax <= 32) {
       this.timelines.splice(0, 1, newTimeline);
@@ -182,8 +201,10 @@ public backToTop() {
   public changeBudgetOnScroll() {
     const budgetMin = this.value[0];
     const budgetMax = this.value[1];
-    const newBudget = ''.concat(('$ ') + budgetMin + ' - ' + ('$') + budgetMax);
-    const newBudgetTop = ''.concat(('$ ') + budgetMin + ' - ' + ('$') + budgetMax + (' and up'));
+    const newBudget = "".concat("$ " + budgetMin + " - " + "$" + budgetMax);
+    const newBudgetTop = "".concat(
+      "$ " + budgetMin + " - " + "$" + budgetMax + " and up"
+    );
 
     if (budgetMax <= 97000) {
       this.budgets.splice(0, 1, newBudget);
