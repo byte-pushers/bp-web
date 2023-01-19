1418c30bf0f9e9ccc2dc8e6545e71d1b4a34d763
Surya Jamalpur <surya.jamalpuri@prolifics.com>
2023-01-19 20:59:03 +0530

    WIP GTM instalation

--- bp-web-client/angular-app/src/app/shared/components/app.component/app.component.ts
+++ bp-web-client/angular-app/src/app/shared/components/app.component/app.component.ts
@@ -1,16 +1,38 @@
-import {Component, OnInit} from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { NavigationEnd, Router } from "@angular/router";
+import { GoogleTagManagerService } from "angular-google-tag-manager";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
-  title = 'angular-app';
-
-  ngOnInit(): void {
-
+  title = "angular-app";
+  constructor(
+    private router: Router,
+    private gtmService: GoogleTagManagerService
+  ) {}
+  ngOnInit() {
+    // push GTM data layer for every visited page
+    this.router.events.forEach((item) => {
+      if (item instanceof NavigationEnd) {
+        const gtmTag = {
+          event: "page",
+          pageName: item.url,
+        };
+        this.gtmService.pushTag(gtmTag);
+      }
+    });
   }
+  customEvent() {
+    // push GTM data layer with a custom event
+    const gtmTag = {
+      event: "button-click",
+      data: "my-custom-event",
+    };
+    this.gtmService.pushTag(gtmTag);
 
+    alert("this is a custom event");
+  }
 }
