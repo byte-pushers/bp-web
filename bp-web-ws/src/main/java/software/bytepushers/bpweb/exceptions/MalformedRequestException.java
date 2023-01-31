3fbf336f2405d43b57aa44a261cea988f23ba040
AmanAb428 <amanjotabroljava@gmail.com>
2023-01-31 20:46:19 +0530

    BP-347: Changes error response structure for MalformedRequestException

--- bp-web-ws/src/main/java/software/bytepushers/bpweb/exceptions/MalformedRequestException.java
+++ bp-web-ws/src/main/java/software/bytepushers/bpweb/exceptions/MalformedRequestException.java
@@ -1,19 +1,19 @@
 package software.bytepushers.bpweb.exceptions;
 
-import software.bytepushers.bpweb.model.dto.ApiErrorResponse;
+import software.bytepushers.bpweb.model.dto.ApiResponse1;
 
 public class MalformedRequestException extends ApplicationMessageException {
 
-    private ApiErrorResponse apiErrorResponse;
+    private ApiResponse1 apiResponse1;
     public MalformedRequestException(String... msgs) {
         super(msgs);
     }
 
-    public MalformedRequestException(ApiErrorResponse apiErrorResponse){
-        this.apiErrorResponse = apiErrorResponse;
+    public MalformedRequestException(ApiResponse1 apiResponse1){
+        this.apiResponse1 = apiResponse1;
     }
 
-    public ApiErrorResponse getApiResponse1() {
-        return apiErrorResponse;
+    public ApiResponse1 getApiResponse1() {
+        return apiResponse1;
     }
 }
