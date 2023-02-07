cf1dba177643d507241c9ff7e888a5eb139597ae
Surya Jamalpur <surya.jamalpuri@prolifics.com>
2023-02-07 09:55:45 +0530

    feature/bp-351 added index html for prod env

--- bp-web-client/angular-app/src/app/shared/components/app.component/app.component.ts
+++ bp-web-client/angular-app/src/app/shared/components/app.component/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { NavigationEnd, Router } from "@angular/router";
 import { GoogleTagManagerService } from "angular-google-tag-manager";
+import { environment } from "src/environments/environment";
 
 @Component({
   selector: "app-root",
@@ -9,6 +10,9 @@ import { GoogleTagManagerService } from "angular-google-tag-manager";
 })
 export class AppComponent implements OnInit {
   title = "angular-app";
-  constructor() {}
+  hubspotTrackingId:string;
+  constructor() {
+    this.hubspotTrackingId= environment.HUBSPOT_TRACKING_ID;
+  }
   ngOnInit() {}
 }
