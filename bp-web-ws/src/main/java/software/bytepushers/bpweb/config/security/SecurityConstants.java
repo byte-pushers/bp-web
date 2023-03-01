0c4d49bce05ecd42d172fbe29096ba6754513015
AmanAb428 <amanjotabroljava@gmail.com>
2023-03-01 10:57:56 +0530

    BP-399: Initial commit of login API

---
+++ bp-web-ws/src/main/java/software/bytepushers/bpweb/config/security/SecurityConstants.java
@@ -0,0 +1,41 @@
+package software.bytepushers.bpweb.config.security;
+
+/**
+ * Security constants.
+ */
+public interface SecurityConstants {
+
+    String HEADER_STRING = "Authorization";
+
+    String TOKEN_PREFIX = "Bearer ";
+
+    int TOKEN_EXPIRY_TIME = 60 * 60 * 1000;
+
+    String JWT_ROLE_JOIN_DELIMITER = ",";
+
+    String ROLE_GUEST = "GUEST";
+
+    String ROLE_BASIC = "BASIC";
+
+    String ROLE_PREMIUM = "PREMIUM";
+
+    String LOGIN_END_POINT = "/api/v1/login";
+
+    String LOGOUT_END_POINT = "/api/v1/logout";
+
+    String USERS_END_POINT = "/api/v1/users";
+
+    String ROLES_END_POINT = "/api/v1/roles";
+
+    String ACCOUNT_TYPE_END_POINT = "/api/v1/account-type";
+
+    String TOKEN_ERROR_ATTRIBUTE_KEY = "tokenError";
+
+    int TOKEN_REFRESH_WINDOW_IN_MINUTES = 5 * 60;
+
+    String JWT_TOKEN_COOKIE_NAME = "jwtToken";
+
+    //TODO: move it to the environment variable
+    String SECRET_KEY = "chuckspick3chuckspick3chuckspick3chuckspick3chuckspick3";
+
+}
