2d3abe608bf5822f02fdad586c68d5e744bf15ea
Surya <jsurya352@gmail.com>
2023-03-01 22:28:47 +0530

    features/bp-366 modified route to admin

--- bp-web-client/angular-app/src/app/app-routing.module.ts
+++ bp-web-client/angular-app/src/app/app-routing.module.ts
@@ -1,34 +1,32 @@
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
-import {HomeComponent} from './components/app-home/app-home.component';
-import {AboutComponent} from './components/app-about/app-about.component';
-import {ContactComponent} from './components/app-contact/app-contact.component';
-import {WorkComponent} from './components/app-work/app-work.component';
-import {ServicesComponent} from './components/app-services/app-services.component';
-import {AppLoginComponent} from './components/app-login/app-login.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./components/app-home/app-home.component";
+import { AboutComponent } from "./components/app-about/app-about.component";
+import { ContactComponent } from "./components/app-contact/app-contact.component";
+import { WorkComponent } from "./components/app-work/app-work.component";
+import { ServicesComponent } from "./components/app-services/app-services.component";
+import { AppLoginComponent } from "./components/app-login/app-login.component";
 
 const routes: Routes = [
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent},
-  {path: 'about', component: AboutComponent},
-  {path: 'contact', component: ContactComponent},
-  {path: 'work', component: WorkComponent},
-  {path: 'services', component: ServicesComponent},
-  {path: 'login', component: AppLoginComponent}
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  { path: "home", component: HomeComponent },
+  { path: "about", component: AboutComponent },
+  { path: "contact", component: ContactComponent },
+  { path: "work", component: WorkComponent },
+  { path: "services", component: ServicesComponent },
+  { path: "admin", component: AppLoginComponent },
 ];
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
 
 export const routingComponents = [
   HomeComponent,
   AboutComponent,
   ContactComponent,
   WorkComponent,
-  ServicesComponent
+  ServicesComponent,
 ];
