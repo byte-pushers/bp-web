a174674fc526463a79eecf221317183f410fd6c5
Surya <jsurya352@gmail.com>
2023-03-02 17:18:20 +0530

    features/bp-366 created Quotes component and added grid from primeNg

---
+++ bp-web-client/angular-app/src/app/components/admin/quotes/quotes.component.ts
@@ -0,0 +1,38 @@
+import { Component, ElementRef, ViewChild } from "@angular/core";
+import { fromEvent, Observable, Subscription } from "rxjs";
+import { ScrollToService } from "src/app/services/scroll-to.service";
+import { environment } from "src/environments/environment";
+import * as $ from "jquery";
+import { Product } from "src/app/shared/models/address";
+import { ProductService } from "src/app/services/quotes.service";
+
+@Component({
+  selector: "app-quotes",
+  templateUrl: "./quotes.component.html",
+  styleUrls: ["./quotes.component.scss"],
+})
+export class QuotesComponent {
+  public chucksPick3Url = environment.CHUCKS_PICK_3_URL;
+
+  constructor(
+    private window: Window,
+    private productService: ProductService,
+    public scrollToService: ScrollToService
+  ) {}
+  products: Product[];
+
+  cols: any[];
+
+  ngOnInit() {
+    this.productService
+      .getProductsSmall()
+      .then((data) => (this.products = data));
+
+    this.cols = [
+      { field: "code", header: "Code" },
+      { field: "name", header: "Name" },
+      { field: "category", header: "Category" },
+      { field: "quantity", header: "Quantity" },
+    ];
+  }
+}
