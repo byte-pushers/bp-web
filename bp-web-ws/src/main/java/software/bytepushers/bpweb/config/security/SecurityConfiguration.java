22d8c57b70f911f719bf467b57e50bfd604d1b17
AmanAb428 <amanjotabroljava@gmail.com>
2023-03-18 13:47:09 +0530

    BP-399: Make the API working

--- bp-web-ws/src/main/java/software/bytepushers/bpweb/config/security/SecurityConfiguration.java
+++ bp-web-ws/src/main/java/software/bytepushers/bpweb/config/security/SecurityConfiguration.java
@@ -22,6 +22,7 @@ import static software.bytepushers.bpweb.config.security.SecurityConstants.*;
  */
 @Log4j2
 @Configuration
+
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private final CustomUserDetailsService customUserDetailsService;
@@ -62,10 +63,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         log.info("Securing the rest endpoints");
-        http.cors().and().csrf().disable().authorizeRequests()
+        http.cors().and().csrf().disable()
+                .authorizeRequests()
                 .antMatchers(LOGIN_END_POINT, ACCOUNT_TYPE_END_POINT, ROLES_END_POINT).permitAll()
                 .antMatchers(HttpMethod.POST, USERS_END_POINT).permitAll()
-                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .antMatchers("/api/**").hasAnyRole(ROLE_PREMIUM, ROLE_BASIC, ROLE_GUEST)
                 .anyRequest().permitAll().and()
                 .exceptionHandling().authenticationEntryPoint(restAuthenticationEntryPoint).and()
