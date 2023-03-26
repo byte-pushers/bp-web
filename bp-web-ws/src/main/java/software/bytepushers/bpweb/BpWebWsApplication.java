bdf4d3fe12271178fd1259a0a627ef7f3f06d305
AmanAb428 <amanjotabroljava@gmail.com>
2023-03-26 11:07:08 +0530

    BP-399: To make build successful

--- bp-web-ws/src/main/java/software/bytepushers/bpweb/BpWebWsApplication.java
+++ bp-web-ws/src/main/java/software/bytepushers/bpweb/BpWebWsApplication.java
@@ -1,13 +1,8 @@
 package software.bytepushers.bpweb;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import software.bytepushers.bpweb.model.dto.enums.AccountTypeEnum;
-import software.bytepushers.bpweb.model.entity.AccountType;
-import software.bytepushers.bpweb.repository.AccountTypeRepository;
 
 /**
  * The spring boot application for the byte pushers web services.
