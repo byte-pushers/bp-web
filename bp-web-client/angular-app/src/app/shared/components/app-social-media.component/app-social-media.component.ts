816cb0aeb1848c44c8d730e8b7da804e058b1dbf
Surya <jsurya352@gmail.com>
2023-04-16 11:36:20 +0530

    refactored social media to component template

--- bp-web-client/angular-app/src/app/shared/components/app-social-media.component/app-social-media.component.ts
+++ bp-web-client/angular-app/src/app/shared/components/app-social-media.component/app-social-media.component.ts
@@ -2,7 +2,57 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-social-media",
-  templateUrl: "./app-social-media.component.html",
-  styleUrls: ["./app-social-media.component.scss"],
+  template: `<ul class="socialMediaLinksTop">
+    <li>
+      <a
+        href="https://facebook.com/bytepushersinc"
+        target="_blank"
+        title="Facebook"
+        rel="noopener noreferrer"
+      >
+        <i class="pi pi-facebook"></i>
+      </a>
+    </li>
+    <li>
+      <a
+        href="https://twitter.com/BytePushersInc"
+        target="_blank"
+        title="Twitter"
+        rel="noopener noreferrer"
+      >
+        <i class="pi pi-twitter"></i>
+      </a>
+    </li>
+    <li>
+      <a
+        href="https://www.instagram.com/bytepushersinc/"
+        target="_blank"
+        title="Instagram"
+        rel="noopener noreferrer"
+      >
+        <i class="pi pi-instagram"></i>
+      </a>
+    </li>
+    <li>
+      <a
+        href="https://www.linkedin.com/company/bytepushersinc/"
+        target="_blank"
+        title="LinkedIn"
+        rel="noopener noreferrer"
+      >
+        <i class="pi pi-linkedin"></i>
+      </a>
+    </li>
+    <li>
+      <a
+        href="https://www.youtube.com/@bytepushersinc"
+        target="_blank"
+        title="Youtube"
+        rel="noopener noreferrer"
+      >
+        <i class="pi pi-youtube"></i>
+      </a>
+    </li>
+  </ul> `,
 })
 export class SocialMediaComponent {}
