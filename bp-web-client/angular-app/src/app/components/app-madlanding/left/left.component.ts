fa4f513da69ac34b4c529fe849f0337b20e9a307
Surya <jsurya352@gmail.com>
2023-04-16 15:47:17 +0530

    left layout is completed

--- bp-web-client/angular-app/src/app/components/app-madlanding/left/left.component.ts
+++ bp-web-client/angular-app/src/app/components/app-madlanding/left/left.component.ts
@@ -1,8 +1,25 @@
 import { Component } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: "app-left",
+  selector: "app-mdn-left",
   templateUrl: "./left.component.html",
   styleUrls: ["./left.component.scss"],
 })
-export class LeftComponent {}
+export class LeftComponent {
+  public ctaForm: FormGroup;
+  constructor() {
+    this.ctaForm = new FormGroup({
+      ctaName: new FormControl("", [Validators.required]),
+      ctaEmail: new FormControl("", [Validators.required, Validators.email]),
+    });
+  }
+
+  onCTASubmit() {
+    let ctaReqObj = {
+      Name: this.ctaForm.controls["ctaName"].value,
+      Email: this.ctaForm.controls["ctaEmail"].value,
+    };
+    console.log(ctaReqObj);
+  }
+}
