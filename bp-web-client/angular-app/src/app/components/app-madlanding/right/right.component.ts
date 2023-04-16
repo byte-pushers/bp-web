a2b4db1b519539356a7d368f36d6fbd84f59244b
Surya <jsurya352@gmail.com>
2023-04-16 15:28:36 +0530

    mad right layout is completed

--- bp-web-client/angular-app/src/app/components/app-madlanding/right/right.component.ts
+++ bp-web-client/angular-app/src/app/components/app-madlanding/right/right.component.ts
@@ -1,8 +1,25 @@
 import { Component } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: "app-right",
+  selector: "app-mdn-right",
   templateUrl: "./right.component.html",
   styleUrls: ["./right.component.scss"],
 })
-export class RightComponent {}
+export class RightComponent {
+  public ctaForm: FormGroup;
+  constructor() {
+    this.ctaForm = new FormGroup({
+      ctaName: new FormControl("", [Validators.required]),
+      ctaEmail: new FormControl("", [Validators.required, Validators.email]),
+    });
+  }
+
+  onCTASubmit() {
+    let ctaReqObj = {
+      Name: this.ctaForm.controls["ctaName"].value,
+      Email: this.ctaForm.controls["ctaEmail"].value,
+    };
+    console.log(ctaReqObj);
+  }
+}
