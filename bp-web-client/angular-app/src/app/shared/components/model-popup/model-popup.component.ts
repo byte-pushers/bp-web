aece7cde68a04b70f4600a6d8a70f368b5af45d8
Surya <jsurya352@gmail.com>
2023-04-20 15:04:39 +0530

    popup ui inprogress

--- bp-web-client/angular-app/src/app/shared/components/model-popup/model-popup.component.ts
+++ bp-web-client/angular-app/src/app/shared/components/model-popup/model-popup.component.ts
@@ -1,8 +1,49 @@
 import { Component } from "@angular/core";
+import { PopupService } from "src/app/services/popup.service";
 
 @Component({
   selector: "app-model-popup",
   templateUrl: "./model-popup.component.html",
   styleUrls: ["./model-popup.component.scss"],
 })
-export class ModelPopupComponent {}
+export class ModelPopupComponent {
+  public popup: any;
+  public isModalPopup: boolean = false;
+  private button1: string = "";
+  private button2: string = "";
+  private button3: string = "";
+  constructor(private popupService: PopupService) {
+    this.popupService.getPopupBroadcast().subscribe((popupInfo: any) => {
+      console.log(popupInfo);
+      this.popup = popupInfo;
+      this.show();
+    });
+  }
+
+  show(): void {
+    this.isModalPopup = true;
+  }
+  hide(): void {
+    this.isModalPopup = false;
+  }
+  confirm() {
+    this.hide();
+    this.popupService.sendResponse(true);
+  }
+  dismiss() {
+    this.hide();
+    this.popupService.sendResponse(false);
+  }
+  close() {
+    this.hide();
+    this.popupService.sendResponse("close");
+  }
+  cancel() {
+    this.hide();
+    this.popupService.sendResponse("cancel");
+  }
+  ok() {
+    this.hide();
+    this.popupService.sendResponse("ok");
+  }
+}
