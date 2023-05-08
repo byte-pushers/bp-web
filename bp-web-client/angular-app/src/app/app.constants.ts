30baae13679ae1fc1a179281861b4a8e269fd2f6
Surya <jsurya352@gmail.com>
2023-05-08 16:24:05 +0530

    added v2 components to constants array to load them dynamically

--- bp-web-client/angular-app/src/app/app.constants.ts
+++ bp-web-client/angular-app/src/app/app.constants.ts
@@ -1,31 +1,80 @@
-import {ComponentType} from "@angular/cdk/portal";
+import { ComponentType } from "@angular/cdk/portal";
 
-export const LANDING_PAGE_LAYOUT_CONFIGURATION: {createComponent, component: ComponentType<any>, inputs: any}[] = [
+export const LANDING_PAGE_LAYOUT_CONFIGURATION: {
+  createComponent;
+  component: ComponentType<any>;
+  inputs: any;
+}[] = [
   {
-    createComponent: ()=> import('../app/components/app-madlanding/left/left.component').then(it => it.LeftComponent),
+    createComponent: () =>
+      import("../app/components/app-madlanding/left/left.component").then(
+        (it) => it.LeftComponent
+      ),
     component: null,
     inputs: {
-      image: 'somepath',
-      title: 'some title',
-      slogan: 'some slogan'
-    }
+      image: "somepath",
+      title: "some title",
+      slogan: "some slogan",
+    },
   },
   {
-    createComponent: ()=> import('../app/components/app-madlanding/right/right.component').then(it => it.RightComponent),
+    createComponent: () =>
+      import("../app/components/app-madlanding/right/right.component").then(
+        (it) => it.RightComponent
+      ),
     component: null,
     inputs: {
-      image: 'somepath',
-      title: 'some title',
-      slogan: 'some slogan'
-    }
+      image: "somepath",
+      title: "some title",
+      slogan: "some slogan",
+    },
   },
   {
-    createComponent: ()=> import('../app/components/app-madlanding/bottom/bottom.component').then(it => it.BottomComponent),
+    createComponent: () =>
+      import("../app/components/app-madlanding/bottom/bottom.component").then(
+        (it) => it.BottomComponent
+      ),
     component: null,
     inputs: {
-      image: 'somepath',
-      title: 'some title',
-      slogan: 'some slogan'
-    }
-  }
+      image: "somepath",
+      title: "some title",
+      slogan: "some slogan",
+    },
+  },
+  {
+    createComponent: () =>
+      import(
+        "../app/components/app-landing-pages/version2/left/left.component"
+      ).then((it) => it.V2LeftComponent),
+    component: null,
+    inputs: {
+      image: "somepath",
+      title: "some title",
+      slogan: "some slogan",
+    },
+  },
+  {
+    createComponent: () =>
+      import(
+        "../app/components/app-landing-pages/version2/right/right.component"
+      ).then((it) => it.V2RightComponent),
+    component: null,
+    inputs: {
+      image: "somepath",
+      title: "some title",
+      slogan: "some slogan",
+    },
+  },
+  {
+    createComponent: () =>
+      import(
+        "../app/components/app-landing-pages/version2/bottom/bottom.component"
+      ).then((it) => it.V2BottomComponent),
+    component: null,
+    inputs: {
+      image: "somepath",
+      title: "some title",
+      slogan: "some slogan",
+    },
+  },
 ];
