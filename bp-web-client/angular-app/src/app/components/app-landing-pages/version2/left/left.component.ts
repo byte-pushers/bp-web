f3773d84f8f47a6b951fbd29d8d7d01c41aa2474
Surya <jsurya352@gmail.com>
2023-05-08 16:26:16 +0530

    created bottom left right components for v2

---
+++ bp-web-client/angular-app/src/app/components/app-landing-pages/version2/left/left.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+
+@Component({
+  selector: "app-mdn-left",
+  templateUrl: "./left.component.html",
+  styleUrls: ["./left.component.scss"],
+})
+export class V2LeftComponent implements OnInit {
+  public ctaForm: FormGroup;
+  constructor() {
+    this.ctaForm = new FormGroup({
+      ctaName: new FormControl("", [Validators.required]),
+      ctaEmail: new FormControl("", [Validators.required, Validators.email]),
+    });
+  }
+
+  ngOnInit() {}
+  onCTASubmit() {
+    let ctaReqObj = {
+      Name: this.ctaForm.controls["ctaName"].value,
+      Email: this.ctaForm.controls["ctaEmail"].value,
+    };
+    console.log(ctaReqObj);
+  }
+}
---
+++ bp-web-client/angular-app/src/app/components/app-landing-pages/version2/right/right.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+
+@Component({
+  selector: "app-mdn-right",
+  templateUrl: "./right.component.html",
+  styleUrls: ["./right.component.scss"],
+})
+export class V2RightComponent {
+  public ctaForm: FormGroup;
+
+  constructor() {
+    this.ctaForm = new FormGroup({
+      ctaName: new FormControl("", [Validators.required]),
+      ctaEmail: new FormControl("", [Validators.required, Validators.email]),
+    });
+  }
+
+  onCTASubmit() {
+    let ctaReqObj = {
+      Name: this.ctaForm.controls["ctaName"].value,
+      Email: this.ctaForm.controls["ctaEmail"].value,
+    };
+    console.log(ctaReqObj);
+  }
+}
