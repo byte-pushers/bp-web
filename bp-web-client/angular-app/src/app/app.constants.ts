f6a2760a7e69e56a6981d4f1bb8633e9f6e45401
Surya <jsurya352@gmail.com>
2023-05-09 23:45:37 +0530

    feat(bp-493) - made some config changes

--- bp-web-client/angular-app/src/app/app.constants.ts
+++ bp-web-client/angular-app/src/app/app.constants.ts
@@ -4,7 +4,19 @@ export const LANDING_PAGE_LAYOUT_CONFIGURATION_MAP: Map<
   string,
   { createComponent; component: ComponentType<any>; inputs: any }[]
 > = new Map();
+let actionItem1Inputs = {
+  image: "somepath",
+  title: "Mobile App Development Made Simple.",
+  slogan:
+    "Mobile App development can be complex, but with Byte Pushers, it doesn't have to be. We'll closely work with you to understand your business goals and create a mobile app that aligns with your vision and budget.",
+};
 
+let actionItem2Inputs = {
+  image: "somepath",
+  title: "Innovative Mobile Solutions, Crafted By Byte Pushers.",
+  slogan:
+    "Byte Pushers builds mobile apps designed to engage your users and drive your business forward. We use the latestmobile technologies and industry best practices to ensure that your app is intuitive, user-friendly and scalable.",
+};
 LANDING_PAGE_LAYOUT_CONFIGURATION_MAP.set("default", [
   {
     createComponent: () =>
@@ -54,12 +66,7 @@ LANDING_PAGE_LAYOUT_CONFIGURATION_MAP.set("1", [
         "../app/components/app-landing-pages/layouts/left/landing-page-left-layout.component"
       ).then((it) => it.LandingPageLeftLayoutComponent),
     component: null,
-    inputs: {
-      image: "somepath",
-      title: "Mobile App Development Made Simple.",
-      slogan:
-        "Mobile App development can be complex, but with Byte Pushers, it doesn't have to be. We'll closely work with you to understand your business goals and create a mobile app that aligns with your vision and budget.",
-    },
+    inputs: actionItem1Inputs,
   },
   {
     createComponent: () =>
@@ -67,12 +74,7 @@ LANDING_PAGE_LAYOUT_CONFIGURATION_MAP.set("1", [
         "../app/components/app-landing-pages/layouts/right/landing-page-right-layout.component"
       ).then((it) => it.LandingPageRightLayoutComponent),
     component: null,
-    inputs: {
-      image: "somepath",
-      title: "Mobile App Development Made Simple.",
-      slogan:
-        "Mobile App development can be complex, but with Byte Pushers, it doesn't have to be. We'll closely work with you to understand your business goals and create a mobile app that aligns with your vision and budget.",
-    },
+    inputs: actionItem1Inputs,
   },
   {
     createComponent: () =>
@@ -80,56 +82,79 @@ LANDING_PAGE_LAYOUT_CONFIGURATION_MAP.set("1", [
         "../app/components/app-landing-pages/layouts/bottom/landing-page-bottom-layout.component"
       ).then((it) => it.LandingPageBottomLayoutComponent),
     component: null,
-    inputs: {
-      image: "somepath",
-      title: "Mobile App Development Made Simple.",
-      slogan:
-        "Mobile App development can be complex, but with Byte Pushers, it doesn't have to be. We'll closely work with you to understand your business goals and create a mobile app that aligns with your vision and budget.",
-    },
+    inputs: actionItem1Inputs,
   },
 ]);
-export const LANDING_PAGE_LAYOUT_CONFIGURATION: {
-  createComponent;
-  component: ComponentType<any>;
-  inputs: any;
-}[] = [
+LANDING_PAGE_LAYOUT_CONFIGURATION_MAP.set("2", [
   {
     createComponent: () =>
-      import("../app/components/app-madlanding/left/left.component").then(
-        (it) => it.LeftComponent
-      ),
+      import(
+        "../app/components/app-landing-pages/layouts/left/landing-page-left-layout.component"
+      ).then((it) => it.LandingPageLeftLayoutComponent),
     component: null,
-    inputs: {
-      image: "somepath",
-      title: "Mobile App Development Made Simple.",
-      slogan:
-        "Mobile App development can be complex, but with Byte Pushers, it doesn't have to be. We'll closely work with you to understand your business goals and create a mobile app that aligns with your vision and budget.",
-    },
+    inputs: actionItem2Inputs,
   },
   {
     createComponent: () =>
-      import("../app/components/app-madlanding/right/right.component").then(
-        (it) => it.RightComponent
-      ),
+      import(
+        "../app/components/app-landing-pages/layouts/right/landing-page-right-layout.component"
+      ).then((it) => it.LandingPageRightLayoutComponent),
     component: null,
-    inputs: {
-      image: "somepath",
-      title: "Mobile App Development Made Simple.",
-      slogan:
-        "Mobile App development can be complex, but with Byte Pushers, it doesn't have to be. We'll closely work with you to understand your business goals and create a mobile app that aligns with your vision and budget.",
-    },
+    inputs: actionItem2Inputs,
   },
   {
     createComponent: () =>
-      import("../app/components/app-madlanding/bottom/bottom.component").then(
-        (it) => it.BottomComponent
-      ),
+      import(
+        "../app/components/app-landing-pages/layouts/bottom/landing-page-bottom-layout.component"
+      ).then((it) => it.LandingPageBottomLayoutComponent),
     component: null,
-    inputs: {
-      image: "somepath",
-      title: "Mobile App Development Made Simple.",
-      slogan:
-        "Mobile App development can be complex, but with Byte Pushers, it doesn't have to be. We'll closely work with you to understand your business goals and create a mobile app that aligns with your vision and budget.",
-    },
+    inputs: actionItem2Inputs,
   },
-];
+]);
+
+// commented this as we are no longer need this
+// export const LANDING_PAGE_LAYOUT_CONFIGURATION: {
+//   createComponent;
+//   component: ComponentType<any>;
+//   inputs: any;
+// }[] = [
+//   {
+//     createComponent: () =>
+//       import("../app/components/app-madlanding/left/left.component").then(
+//         (it) => it.LeftComponent
+//       ),
+//     component: null,
+//     inputs: {
+//       image: "somepath",
+//       title: "Mobile App Development Made Simple.",
+//       slogan:
+//         "Mobile App development can be complex, but with Byte Pushers, it doesn't have to be. We'll closely work with you to understand your business goals and create a mobile app that aligns with your vision and budget.",
+//     },
+//   },
+//   {
+//     createComponent: () =>
+//       import("../app/components/app-madlanding/right/right.component").then(
+//         (it) => it.RightComponent
+//       ),
+//     component: null,
+//     inputs: {
+//       image: "somepath",
+//       title: "Mobile App Development Made Simple.",
+//       slogan:
+//         "Mobile App development can be complex, but with Byte Pushers, it doesn't have to be. We'll closely work with you to understand your business goals and create a mobile app that aligns with your vision and budget.",
+//     },
+//   },
+//   {
+//     createComponent: () =>
+//       import("../app/components/app-madlanding/bottom/bottom.component").then(
+//         (it) => it.BottomComponent
+//       ),
+//     component: null,
+//     inputs: {
+//       image: "somepath",
+//       title: "Mobile App Development Made Simple.",
+//       slogan:
+//         "Mobile App development can be complex, but with Byte Pushers, it doesn't have to be. We'll closely work with you to understand your business goals and create a mobile app that aligns with your vision and budget.",
+//     },
+//   },
+// ];
