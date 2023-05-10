8469051ab087e96bd17db36c79ea5efa84198ce8
Surya <jsurya352@gmail.com>
2023-05-10 17:05:00 +0530

    removed v3 as it doesn't require

--- bp-web-client/angular-app/src/app/components/app-landing-pages/version3/version3.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "app-version3",
-  templateUrl: "./version3.component.html",
-  styleUrls: ["./version3.component.scss"],
-})
-export class Version3Component {}
