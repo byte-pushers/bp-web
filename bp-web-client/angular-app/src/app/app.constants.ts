b298a44149ad024b0de620741bef47e7e50b18b1
Surya <jsurya352@gmail.com>
2023-05-10 16:57:07 +0530

    added theme configurations

--- bp-web-client/angular-app/src/app/app.constants.ts
+++ bp-web-client/angular-app/src/app/app.constants.ts
@@ -9,6 +9,15 @@ let actionItem1Inputs = {
   title: "Mobile App Development Made Simple.",
   slogan:
     "Mobile App development can be complex, but with Byte Pushers, it doesn't have to be. We'll closely work with you to understand your business goals and create a mobile app that aligns with your vision and budget.",
+  theme: {
+    primaryColor: "#fff",
+    secondaryColor: "#fff",
+    imagePath: {
+      right: "assets/images/landingPages/v1/vector/right-align.png",
+      left: "assets/images/landingPages/v1/vector/left-align.png",
+      bottom: "assets/images/landingPages/v1/vector/bottom-align.png",
+    },
+  },
 };
 
 let actionItem2Inputs = {
@@ -16,6 +25,15 @@ let actionItem2Inputs = {
   title: "Innovative Mobile Solutions, Crafted By Byte Pushers.",
   slogan:
     "Byte Pushers builds mobile apps designed to engage your users and drive your business forward. We use the latestmobile technologies and industry best practices to ensure that your app is intuitive, user-friendly and scalable.",
+  theme: {
+    primaryColor: "#0263bb",
+    secondaryColor: "#000",
+    imagePath: {
+      right: "assets/images/landingPages/v2/vector/right-align.png",
+      left: "assets/images/landingPages/v2/vector/left-align.png",
+      bottom: "assets/images/landingPages/v2/vector/bottom-align.png",
+    },
+  },
 };
 LANDING_PAGE_LAYOUT_CONFIGURATION_MAP.set("default", [
   {
@@ -25,35 +43,38 @@ LANDING_PAGE_LAYOUT_CONFIGURATION_MAP.set("default", [
       ).then((it) => it.LandingPageBottomLayoutComponent),
     component: null,
     inputs: {
-      image: 'somepath',
+      image: "somepath",
       title: null,
-      slogan: 'Creating solutions to solve today\'s and tomorrow’s problems <strong>bit</strong> by <strong>bit</strong>.'
+      slogan:
+        "Creating solutions to solve today's and tomorrow’s problems <strong>bit</strong> by <strong>bit</strong>.",
     },
   },
   {
     createComponent: () =>
       import(
         "../app/components/app-landing-pages/layouts/right/landing-page-right-layout.component"
-        ).then((it) => it.LandingPageRightLayoutComponent),
+      ).then((it) => it.LandingPageRightLayoutComponent),
     component: null,
     inputs: {
-      image: 'somepath',
+      image: "somepath",
       title: null,
-      slogan: 'Creating solutions to solve today\'s and tomorrow’s problems <strong>bit</strong> by <strong>bit</strong>.'
+      slogan:
+        "Creating solutions to solve today's and tomorrow’s problems <strong>bit</strong> by <strong>bit</strong>.",
     },
   },
   {
     createComponent: () =>
       import(
         "../app/components/app-landing-pages/layouts/left/landing-page-left-layout.component"
-        ).then((it) => it.LandingPageLeftLayoutComponent),
+      ).then((it) => it.LandingPageLeftLayoutComponent),
     component: null,
     inputs: {
-      image: 'somepath',
+      image: "somepath",
       title: null,
-      slogan: 'Creating solutions to solve today\'s and tomorrow’s problems <strong>bit</strong> by <strong>bit</strong>.'
+      slogan:
+        "Creating solutions to solve today's and tomorrow’s problems <strong>bit</strong> by <strong>bit</strong>.",
     },
-  }
+  },
 ]);
 
 LANDING_PAGE_LAYOUT_CONFIGURATION_MAP.set("1", [
