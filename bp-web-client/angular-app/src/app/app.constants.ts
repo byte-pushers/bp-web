658e611e7124bae2b37c71e9ac189cf5544cb65f
Surya <jsurya352@gmail.com>
2023-05-16 18:52:58 +0530

    removed default configuration in app.constants as we no longer needed

--- bp-web-client/angular-app/src/app/app.constants.ts
+++ bp-web-client/angular-app/src/app/app.constants.ts
@@ -6,50 +6,6 @@ export const LANDING_PAGE_LAYOUT_CONFIGURATION_MAP: Map<
   { createComponent; component: ComponentType<any>; inputs: any }[]
 > = new Map();
 
-const leftLayoutPath =
-  "../app/components/app-landing-pages/layouts/left/landing-page-left-layout.component";
-
-LANDING_PAGE_LAYOUT_CONFIGURATION_MAP.set("default", [
-  {
-    createComponent: () =>
-      import(
-        "../app/components/app-landing-pages/layouts/bottom/landing-page-bottom-layout.component"
-      ).then((it) => it.LandingPageBottomLayoutComponent),
-    component: null,
-    inputs: {
-      image: "somepath",
-      title:
-        "Creating solutions to solve today's and tomorrow's problems <strong>bit</strong> by <strong>bit</strong>.",
-      slogan: null,
-    },
-  },
-  {
-    createComponent: () =>
-      import(
-        "../app/components/app-landing-pages/layouts/right/landing-page-right-layout.component"
-      ).then((it) => it.LandingPageRightLayoutComponent),
-    component: null,
-    inputs: {
-      image: "somepath",
-      title:
-        "Creating solutions to solve today's and tomorrow's problems <strong>bit</strong> by <strong>bit</strong>.",
-      slogan: null,
-    },
-  },
-  {
-    createComponent: () =>
-      import(
-        "../app/components/app-landing-pages/layouts/left/landing-page-left-layout.component"
-      ).then((it) => it.LandingPageLeftLayoutComponent),
-    component: null,
-    inputs: {
-      image: "somepath",
-      title:
-        "Creating solutions to solve today's and tomorrow's problems <strong>bit</strong> by <strong>bit</strong>.",
-      slogan: null,
-    },
-  },
-]);
 for (let landingPage of landingPages) {
   LANDING_PAGE_LAYOUT_CONFIGURATION_MAP.set(landingPage.id.toString(), [
     {
