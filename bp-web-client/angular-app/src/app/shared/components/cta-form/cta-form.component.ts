265f232fbae3dc635c31f1c46295f8c4a1b4e98a
Surya <jsurya352@gmail.com>
2023-06-08 12:46:44 +0530

    WIP with the cta form to make it more eficient

--- bp-web-client/angular-app/src/app/shared/components/cta-form/cta-form.component.ts
+++ bp-web-client/angular-app/src/app/shared/components/cta-form/cta-form.component.ts
@@ -1,8 +1,41 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { ActivatedRoute } from "@angular/router";
+import { HeaderService } from "src/app/services/header.service";
 
 @Component({
   selector: "app-cta-form",
   templateUrl: "./cta-form.component.html",
   styleUrls: ["./cta-form.component.scss"],
 })
-export class CTAFormComponent {}
+export class CTAFormComponent {
+  public isConsentModal: boolean = true;
+  @Input() theme;
+  @Input() layout;
+  public ctaForm: FormGroup;
+
+  constructor(
+    private headerService: HeaderService,
+    private route: ActivatedRoute
+  ) {
+    this.ctaForm = new FormGroup({
+      ctaName: new FormControl("", [Validators.required]),
+      ctaEmail: new FormControl("", [Validators.required, Validators.email]),
+    });
+  }
+
+  setThemeSecondaryColor() {
+    let styles = {
+      color: this?.theme?.secondaryColor,
+    };
+    return styles;
+  }
+  setConcent(event: any) {}
+  onCTASubmit() {
+    let ctaReqObj = {
+      Name: this?.ctaForm?.controls["ctaName"]?.value,
+      Email: this?.ctaForm?.controls["ctaEmail"]?.value,
+    };
+    console.log(ctaReqObj);
+  }
+}
