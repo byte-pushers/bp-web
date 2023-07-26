1e35c90193bf78ea182b567ac2ff1b5408a5b012
Surya <jsurya352@gmail.com>
2023-07-26 13:17:51 +0530

    feat(BP-572) -  created sitemap basic component

--- bp-web-client/angular-app/src/app/components/sitemap/sitemap.component.ts
+++ bp-web-client/angular-app/src/app/components/sitemap/sitemap.component.ts
@@ -1,8 +1,27 @@
-import { Component } from "@angular/core";
-
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import * as $ from "jquery";
+// import { faFacebook } from "@fortawesome/free-solid-svg-icons";
+import {
+  faFacebook,
+  faTwitter,
+  faInstagram,
+  faLinkedin,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+import { ActivatedRoute } from "@angular/router";
+import { SiteMapService } from "src/app/services/sitemap.service";
 @Component({
   selector: "app-sitemap",
   templateUrl: "./sitemap.component.html",
   styleUrls: ["./sitemap.component.scss"],
 })
-export class SitemapComponent {}
+export class SitemapComponent implements OnInit, OnDestroy {
+  constructor(private siteMapService: SiteMapService) {}
+
+  ngOnInit() {
+    this.siteMapService.setSiteMap(false);
+  }
+  ngOnDestroy() {
+    this.siteMapService.setSiteMap(true);
+  }
+}
