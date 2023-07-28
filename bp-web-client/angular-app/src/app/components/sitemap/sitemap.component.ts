f8b84ab79da557ea450cac0cbe5a0b0ddd028dbf
Surya <jsurya352@gmail.com>
2023-07-28 13:00:57 +0530

    feat(BP-572) -   created sitemap links dynamically

--- bp-web-client/angular-app/src/app/components/sitemap/sitemap.component.ts
+++ bp-web-client/angular-app/src/app/components/sitemap/sitemap.component.ts
@@ -1,27 +1,29 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
-import * as $ from "jquery";
-// import { faFacebook } from "@fortawesome/free-solid-svg-icons";
-import {
-  faFacebook,
-  faTwitter,
-  faInstagram,
-  faLinkedin,
-  faYoutube,
-} from "@fortawesome/free-brands-svg-icons";
-import { ActivatedRoute } from "@angular/router";
+import landingPages from "../../landing-pages.json";
 import { SiteMapService } from "src/app/services/sitemap.service";
+import { Router } from "@angular/router";
 @Component({
   selector: "app-sitemap",
   templateUrl: "./sitemap.component.html",
   styleUrls: ["./sitemap.component.scss"],
 })
 export class SitemapComponent implements OnInit, OnDestroy {
-  constructor(private siteMapService: SiteMapService) {}
+  landingPagesList = landingPages;
+  constructor(private siteMapService: SiteMapService, private router: Router) {}
 
   ngOnInit() {
     this.siteMapService.setSiteMap(false);
   }
   ngOnDestroy() {
     this.siteMapService.setSiteMap(true);
+  }
+  gotothisroute(page: string, layoutId: number, layoutType?: any) {
+    if (layoutType != undefined) {
+      this.router.navigate([page], {
+        queryParams: { id: layoutId, layout: layoutType },
+      });
+    } else {
+      this.router.navigate([page], { queryParams: { id: layoutId } });
+    }
   }
 }
