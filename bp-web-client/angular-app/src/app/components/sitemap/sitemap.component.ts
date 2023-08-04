e5bfc1562b9f791748683c54db9b0b4c703ec1e7
Surya <jsurya352@gmail.com>
2023-08-04 21:03:18 +0530

    Feature/bp 572 (#338)
    * feat(BP-572) -  figuredout the font for all desktop resolution (>768)
    
    * feat(BP-572) -  added w20-80 for content box
    
    * feat(BP-572) -  WIP changes
    
    * feat(BP-572) -  removed unused code and components
    
    * feat(BP-572) -  created mobile layout component
    
    * feat(BP-572) -  integrated mobile layout and working fine
    
    * feat(BP-572) -  aligned content in mobile
    
    * feat(BP-572) -  changed the alignment approach
    
    * feat(BP-572) -  formated some more
    
    * feat(BP-572) -  formated index htmls
    
    * feat(BP-572) -  formated index htmls
    
    * feat(BP-572) -  alignments for only mobile
    
    * feat(BP-572) -  changed body content columns to 35-65
    
    * feat(BP-572) -  created sitemap component
    
    * feat(BP-572) -  created sitemap route
    
    * feat(BP-572) -  created sitemap base template
    
    * feat(BP-572) -  created sitemap basic component
    
    * feat(BP-572) -  created sitemap service
    
    * feat(BP-572) -  added like for sitemap
    
    * feat(BP-572) -  added logic for show hide header and footer in sitemap page
    
    * feat(BP-572) -   created sitemap links dynamically
    
    * feat(BP-572) -   added small logo for mobile
    
    * feat(BP-572) -   added btn for bottom layout and aligned properly

---
+++ bp-web-client/angular-app/src/app/components/sitemap/sitemap.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import landingPages from "../../landing-pages.json";
+import { SiteMapService } from "src/app/services/sitemap.service";
+import { Router } from "@angular/router";
+@Component({
+  selector: "app-sitemap",
+  templateUrl: "./sitemap.component.html",
+  styleUrls: ["./sitemap.component.scss"],
+})
+export class SitemapComponent implements OnInit, OnDestroy {
+  landingPagesList = landingPages;
+  constructor(private siteMapService: SiteMapService, private router: Router) {}
+
+  ngOnInit() {
+    this.siteMapService.setSiteMap(false);
+  }
+  ngOnDestroy() {
+    this.siteMapService.setSiteMap(true);
+  }
+  gotothisroute(page: string, layoutId: number, layoutType?: any) {
+    if (layoutType != undefined) {
+      this.router.navigate([page], {
+        queryParams: { id: layoutId, layout: layoutType },
+      });
+    } else {
+      this.router.navigate([page], { queryParams: { id: layoutId } });
+    }
+  }
+
+  showSmallLogo() {
+    if (window.innerWidth <= 960) {
+      return true;
+    }
+    return false;
+  }
+}
