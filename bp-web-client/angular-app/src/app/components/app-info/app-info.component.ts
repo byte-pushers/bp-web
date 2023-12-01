1aa282138008090ba79f6e72a5cca4ad8964f592
Surya <jsurya352@gmail.com>
2023-12-01 14:52:39 +0530

    windowref is used instead of window in info page

--- bp-web-client/angular-app/src/app/components/app-info/app-info.component.ts
+++ bp-web-client/angular-app/src/app/components/app-info/app-info.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from "@angular/core";
-
+import { PLATFORM_ID, Inject } from "@angular/core";
+import { isPlatformBrowser } from "@angular/common";
+import { WindowRef } from "src/app/services/windowRef.service";
 @Component({
   selector: "app-info",
   templateUrl: "./app-info.component.html",
   styleUrls: ["./app-info.component.css"],
 })
 export class InfoComponent implements OnInit {
-  constructor() {}
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: any,
+    private windowRef: WindowRef
+  ) {}
   ngOnInit() {}
 
   public toFeaturesSection() {
-    window.scrollTo(0, 900);
+    if (!isPlatformBrowser(this.platformId)) {
+      window.scrollTo(0, 900);
+    }
   }
 }
