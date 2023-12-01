2c9b2f33fe061d93ca82d94201a6213aebcbe3f4
Surya <jsurya352@gmail.com>
2023-12-01 16:53:42 +0530

    windowref is used instead of window in app

--- bp-web-client/angular-app/src/app/shared/components/app.component/app.component.ts
+++ bp-web-client/angular-app/src/app/shared/components/app.component/app.component.ts
@@ -1,9 +1,17 @@
-import {AfterViewInit, ChangeDetectorRef, Component, HostListener, OnInit} from "@angular/core";
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  HostListener,
+  OnInit,
+} from "@angular/core";
 import { Observable } from "rxjs";
 import { SiteMapService } from "src/app/services/sitemap.service";
 import { DEVICE_PLATFORM } from "../../models/screen-size.enum";
 import { ResizeService } from "../../services/resize.service";
-
+import { PLATFORM_ID, Inject } from "@angular/core";
+import { isPlatformBrowser } from "@angular/common";
+import { WindowRef } from "src/app/services/windowRef.service";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
@@ -16,7 +24,9 @@ export class AppComponent implements OnInit, AfterViewInit {
   constructor(
     private siteMapService: SiteMapService,
     private resizeService: ResizeService,
-    private cd: ChangeDetectorRef
+    private cd: ChangeDetectorRef,
+    @Inject(PLATFORM_ID) private platformId: any,
+    private windowRef: WindowRef
   ) {}
 
   @HostListener("window:resize", [])
@@ -25,7 +35,7 @@ export class AppComponent implements OnInit, AfterViewInit {
   }
 
   ngOnInit() {
-    this.isSiteMap$.subscribe(isSiteMap => {
+    this.isSiteMap$.subscribe((isSiteMap) => {
       this.isSiteMap = isSiteMap;
       this.cd.detectChanges();
     });
@@ -34,11 +44,18 @@ export class AppComponent implements OnInit, AfterViewInit {
   ngAfterViewInit() {
     this.#detectScreenSize();
   }
-
+  public windowWidth;
+  setWindowCheck() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.windowWidth = this.windowRef.nativeWindow.innerWidth;
+    } else {
+      this.windowWidth = window.innerWidth;
+    }
+  }
   #detectScreenSize() {
-    if (window.innerWidth <= 820) {
+    if (this.windowWidth <= 820) {
       this.resizeService.onResize(DEVICE_PLATFORM.MOBILE);
-    } else if (window.innerWidth < 1280) {
+    } else if (this.windowWidth < 1280) {
       this.resizeService.onResize(DEVICE_PLATFORM.TABLET);
     } else {
       this.resizeService.onResize(DEVICE_PLATFORM.DESKTOP);
