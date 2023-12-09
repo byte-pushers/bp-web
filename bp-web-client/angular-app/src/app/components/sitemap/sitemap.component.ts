91916b4f3e9a6a44941be9dce92864d1e438f79f
Surya <jsurya352@gmail.com>
2023-12-09 15:00:15 +0530

    set test integration is done

--- bp-web-client/angular-app/src/app/components/sitemap/sitemap.component.ts
+++ bp-web-client/angular-app/src/app/components/sitemap/sitemap.component.ts
@@ -7,6 +7,8 @@ import { isPlatformBrowser } from "@angular/common";
 import { WindowRef } from "src/app/services/windowRef.service";
 import { getWindow, getDocument } from "ssr-window";
 
+import { Meta, Title } from "@angular/platform-browser";
+
 @Component({
   selector: "app-sitemap",
   templateUrl: "./sitemap.component.html",
@@ -17,13 +19,23 @@ export class SitemapComponent implements OnInit, OnDestroy {
   window = getWindow();
   document = getDocument();
   constructor(
+    private meta: Meta,
+    private title: Title,
     private siteMapService: SiteMapService,
-    private router: Router // @Inject(PLATFORM_ID) private platformId: any,
-  ) // private windowRef: WindowRef
-  {}
+    private router: Router // @Inject(PLATFORM_ID) private platformId: any, // private windowRef: WindowRef
+  ) {
+    this.meta.addTags([
+      { name: "description", content: "Home page of SEO friendly app" },
+      { name: "author", content: "redthemes" },
+      { name: "keywords", content: "Angular, redthemes" },
+    ]);
+    this.title.setTitle("Sitemap page");
+  }
 
   ngOnInit() {
     this.siteMapService.setSiteMap(false);
+    const description = this.meta.getTag("name=description");
+    console.log(description.content); //this is to test
   }
   ngOnDestroy() {
     this.siteMapService.setSiteMap(true);
