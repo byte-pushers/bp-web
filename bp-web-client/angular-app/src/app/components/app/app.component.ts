61769fa5b6f77d3f2e998c31b456e04121281d61
Tonté Pouncil <tonte.pouncil@bytepushers.software>
2024-08-19 14:25:52 -0500

    feat(release-0.0.0.1-candidate:home-page-no-cta): wip - trying to get defaut theme dipslaying background image

--- bp-web-client/angular-app/src/app/components/app/app.component.ts
+++ bp-web-client/angular-app/src/app/components/app/app.component.ts
@@ -10,4 +10,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angular-app';
+
+  constructor() {
+    console.log('AppComponent(): inside constructor');
+  }
+
 }
--- bp-web-client/angular-app/tailwind.config.js
+++ bp-web-client/angular-app/tailwind.config.js
@@ -1,10 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
 module.exports = {
+  mode: "jit",
   content: [
-    "./src/**/*.{html,ts}",
+    "./src/**/*.{html,js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Build your palette here
+        transparent: "transparent",
+        gray: colors.slate,
+        blue: colors.blue,
+        lightblue: colors.sky,
+        red: colors.red,
+        green: colors.green,
+        indigo: colors.indigo,
+        white: colors.white,
+      },
+      zIndex: {
+        100: "100",
+      }
+    },
   },
   plugins: [],
 }
