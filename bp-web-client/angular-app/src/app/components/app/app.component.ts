ab9e2e610e14bd6bf58cc7749054c018c77712f5
Tonté Pouncil <tonte.pouncil@bytepushers.software>
2024-08-20 23:18:15 -0500

    feat(release-0.0.0.1-candidate:home-page-no-cta): wip - got close button showing up when menu is opened

--- bp-web-client/angular-app/src/app/components/app/app.component.ts
+++ bp-web-client/angular-app/src/app/components/app/app.component.ts
@@ -1,25 +1,27 @@
 import { Component, Inject, InjectionToken } from '@angular/core';
-import { DOCUMENT } from '@angular/common';
+import { DOCUMENT, NgIf } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular-app';
+  menuState = 'closed';
 
   constructor(@Inject(DOCUMENT) private document: Document) {
     console.log('AppComponent(): inside constructor');
   }
 
   //const navLinks = document.querySelector('.nav-links')
-  onToggleMenu(e: any){
+  toggleMenu(e: any){
     const navLinks = document.querySelector('.nav-links');
-    e.name = e.name === 'menu' ? 'close' : 'menu'
+    // e.name = e.name === 'menu' ? 'close' : 'menu' // if we use ionic icon, as it has name attribute
+    this.menuState = this.menuState === 'closed' ? 'opened' : 'closed'
     navLinks?.classList.toggle('top-[9%]')
   }
 
