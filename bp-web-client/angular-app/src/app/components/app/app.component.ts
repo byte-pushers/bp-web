4bdaa5a269f62c3d34b07051c0a07937b731f16a
Tonté Pouncil <tonte.pouncil@bytepushers.software>
2024-08-24 15:40:21 -0500

    feat(release-0.0.0.1-candidate:home-page-no-cta): wip - refactord menu state

--- bp-web-client/angular-app/src/app/components/app/app.component.ts
+++ bp-web-client/angular-app/src/app/components/app/app.component.ts
@@ -1,18 +1,18 @@
 import { Component, Inject, InjectionToken } from '@angular/core';
-import { DOCUMENT, NgIf } from '@angular/common';
+import { DOCUMENT, NgClass, NgIf } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { SocialMediaComponent } from '../social-media.component/app-social-media.component';
+import { SocialMediaComponent } from '../social-media/social-media.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgIf, SocialMediaComponent],
+  imports: [RouterOutlet, NgIf, SocialMediaComponent, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular-app';
-  menuState = 'closed';
+  isOpen = false;
 
   constructor(@Inject(DOCUMENT) private document: Document) {
     console.log('AppComponent(): inside constructor');
@@ -22,8 +22,7 @@ export class AppComponent {
   toggleMenu(e: any){
     const navLinks = document.querySelector('.nav-links');
     // e.name = e.name === 'menu' ? 'close' : 'menu' // if we use ionic icon, as it has name attribute
-    this.menuState = this.menuState === 'closed' ? 'opened' : 'closed'
+    this.isOpen = !this.isOpen;
     navLinks?.classList.toggle('top-[9%]')
   }
-
 }
