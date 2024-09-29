321543a23e91710507f5cd55a187d99cc46a13bb
Surya Jamalpur <surya.jamalpuri@prolifics.com>
2024-09-29 07:55:51 +0530

    feat(release-0.0.0.1-candidate:home-page-no-cta): Created and invoked tilster logo component and applied theme color

--- bp-web-client/angular-app/src/app/shared/components/companies-we-keep/companies-we-keep.component.ts
+++ bp-web-client/angular-app/src/app/shared/components/companies-we-keep/companies-we-keep.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { VartecLogoComponent } from './vartec-logo/vartec-logo.component';
 import { VALogoComponent } from './va-logo/va-logo.component';
+import { TilsterLogoComponent } from './tilster-logo/tilster-logo.component';
 
 @Component({
   selector: 'app-companies-we-keep',
   standalone: true,
-  imports: [VartecLogoComponent, VALogoComponent],
+  imports: [VartecLogoComponent, VALogoComponent, TilsterLogoComponent],
   templateUrl: './companies-we-keep.component.html',
   styleUrl: './companies-we-keep.component.scss',
 })
