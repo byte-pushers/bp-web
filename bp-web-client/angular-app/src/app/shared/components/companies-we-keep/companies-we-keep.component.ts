836b889d51ed870e8537f8e8068c4161118613f6
Surya Jamalpur <surya.jamalpuri@prolifics.com>
2024-09-29 07:50:36 +0530

    feat(release-0.0.0.1-candidate:home-page-no-cta): Created VA logo component and applied theme color

--- bp-web-client/angular-app/src/app/shared/components/companies-we-keep/companies-we-keep.component.ts
+++ bp-web-client/angular-app/src/app/shared/components/companies-we-keep/companies-we-keep.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { VartecLogoComponent } from './vartec-logo/vartec-logo.component';
+import { VALogoComponent } from './va-logo/va-logo.component';
 
 @Component({
   selector: 'app-companies-we-keep',
   standalone: true,
-  imports: [VartecLogoComponent],
+  imports: [VartecLogoComponent, VALogoComponent],
   templateUrl: './companies-we-keep.component.html',
   styleUrl: './companies-we-keep.component.scss',
 })
