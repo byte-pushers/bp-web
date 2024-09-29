592e4224641233dcd295db3289a8e79f5a40602f
Surya Jamalpur <surya.jamalpuri@prolifics.com>
2024-09-29 08:05:29 +0530

    feat(release-0.0.0.1-candidate:home-page-no-cta): Created Texas Instruments logo and applied theme color

--- bp-web-client/angular-app/src/app/shared/components/companies-we-keep/companies-we-keep.component.ts
+++ bp-web-client/angular-app/src/app/shared/components/companies-we-keep/companies-we-keep.component.ts
@@ -3,6 +3,7 @@ import { VartecLogoComponent } from './vartec-logo/vartec-logo.component';
 import { VALogoComponent } from './va-logo/va-logo.component';
 import { TilsterLogoComponent } from './tilster-logo/tilster-logo.component';
 import { ThomsonReutersLogoComponent } from './thomson-reuters-logo/thomson-reuters-logo.component';
+import { TexasInstrumentsLogoComponent } from './texas-instruments-logo/texas-instruments-logo.component';
 
 @Component({
   selector: 'app-companies-we-keep',
@@ -12,6 +13,7 @@ import { ThomsonReutersLogoComponent } from './thomson-reuters-logo/thomson-reut
     VALogoComponent,
     TilsterLogoComponent,
     ThomsonReutersLogoComponent,
+    TexasInstrumentsLogoComponent,
   ],
   templateUrl: './companies-we-keep.component.html',
   styleUrl: './companies-we-keep.component.scss',
