ee7dc8e2824f09a766775de9e6b4dcab0b8eed4e
Tonté Pouncil <tonte.pouncil@bytepushers.software>
2024-10-20 00:52:46 -0500

    feat(rc-0.0.0.1-dynamic-layout-and-boarders): wip - initial check-in

--- bp-web-client/angular-app/src/app/app.routes.ts
+++ bp-web-client/angular-app/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from '@components/home/home.component';
+import { AppHomeComponent } from '@components/app-home/app-home.component';
 
 export const routes: Routes = [
   { path: "", redirectTo: "/home", pathMatch: "full" },
-  { path: "home", component: HomeComponent }
+  { path: "home", component: AppHomeComponent }
 ];
---
+++ bp-web-client/angular-app/src/app/components/app-landing-pages/layouts/bottom/landing-page-bottom-layout.component.ts
@@ -0,0 +1,45 @@
+import { AfterViewInit, Component, Inject, Input, OnInit } from "@angular/core";
+import { ActivatedRoute, RouterOutlet } from "@angular/router";
+import { DOCUMENT, NgClass, NgIf, NgStyle } from '@angular/common';
+import { SocialMediaComponent } from '@components/social-media/social-media.component';
+import { WINDOW } from '@services/windows/window';
+import { Title } from '@angular/platform-browser';
+
+@Component({
+  selector: "app-landing-page-bottom-layout",
+  templateUrl: "./landing-page-bottom-layout.component.html",
+  styleUrls: ["./landing-page-bottom-layout.component.scss"],
+  imports: [RouterOutlet, NgClass, NgIf, SocialMediaComponent, NgStyle],
+  standalone: true
+})
+export class LandingPageBottomLayoutComponent implements OnInit, AfterViewInit{
+  @Input() heroContent: any;
+  public borderVisible = false;
+
+  constructor(
+    @Inject(WINDOW) private window: Window, @Inject(DOCUMENT) private document: Document, private title: Title,
+    private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.title.setTitle(this.heroContent);
+    this.route.queryParams.subscribe((params) => {
+      if (params?.['showBorders'] == "true") {
+        this.borderVisible = (/true/i).test(params?.['showBorders']);
+      }
+    });
+  }
+
+  ngAfterViewInit() {
+
+  }
+
+  showBorders(): string {
+    let style = '';
+
+    if (this.borderVisible) {
+      style = 'showBorders'
+    }
+
+    return style;
+  }
+}
