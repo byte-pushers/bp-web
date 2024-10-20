87437f42a0ca569aa68e845337d8e7f9fa05d49f
Tonté Pouncil <tonte.pouncil@bytepushers.software>
2024-10-20 00:52:46 -0500

    feat(rc-0.0.0.1-dynamic-layout-and-boarders): wip - initial check-in

---
+++ bp-web-client/angular-app/src/app/components/app-landing-pages/layouts/mobile/mobile.component.ts
@@ -0,0 +1,36 @@
+import { Component, Inject, Input, OnInit } from "@angular/core";
+import { ActivatedRoute, RouterOutlet } from "@angular/router";
+import { DOCUMENT, NgClass, NgIf } from '@angular/common';
+import { SocialMediaComponent } from '@components/social-media/social-media.component';
+import { WINDOW } from '@services/windows/window';
+import { Title } from '@angular/platform-browser';
+
+@Component({
+  selector: "app-mobile",
+  templateUrl: "./mobile.component.html",
+  styleUrls: ["./mobile.component.scss"],
+  imports: [RouterOutlet, NgClass, NgIf, SocialMediaComponent],
+  standalone: true
+})
+export class MobileComponent implements OnInit {
+  @Input() heroContent: any;
+  constructor(
+    @Inject(WINDOW) private window: Window, @Inject(DOCUMENT) private document: Document,
+    private route: ActivatedRoute,
+    private title: Title
+  ) {}
+
+  ngOnInit(): void {
+    this.title.setTitle(this.heroContent);
+  }
+
+  showBorders(): boolean | void {
+    let isBorders;
+    this.route.queryParams.subscribe((params) => {
+      if (params?.['showBorder'] == "true") {
+        isBorders = params?.['showBorder'];
+      }
+    });
+    return isBorders;
+  }
+}
