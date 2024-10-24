3dad24fc02b4da558aacabad1243ec06df928d30
Surya <jsurya352@gmail.com>
2024-10-24 13:21:38 +0530

    feat(release-0.0.0.1-candidate:home-page-no-cta): setup completed for right layout

--- bp-web-client/angular-app/src/app/components/app-landing-pages/layouts/right/landing-page-right-layout.component.ts
+++ bp-web-client/angular-app/src/app/components/app-landing-pages/layouts/right/landing-page-right-layout.component.ts
@@ -1,36 +1,40 @@
 import { Component, Inject, Input, OnInit } from "@angular/core";
 import { ActivatedRoute, RouterOutlet } from "@angular/router";
-import { DOCUMENT, NgClass, NgIf } from '@angular/common';
+import { DOCUMENT, NgClass, NgIf, NgStyle } from '@angular/common';
 import { SocialMediaComponent } from '@components/social-media/social-media.component';
 import { WINDOW } from '@services/windows/window';
 import { Title } from '@angular/platform-browser';
+import { CompaniesWeKeepComponent } from "@app/shared/components/companies-we-keep/companies-we-keep.component";
 
 @Component({
   selector: "app-landing-page-right-layout",
   templateUrl: "./landing-page-right-layout.component.html",
   styleUrls: ["./landing-page-right-layout.component.scss"],
-  imports: [RouterOutlet, NgClass, NgIf, SocialMediaComponent],
+  imports: [RouterOutlet, NgClass, NgIf, SocialMediaComponent, NgStyle, CompaniesWeKeepComponent],
   standalone: true
 })
 export class LandingPageRightLayoutComponent implements OnInit {
+  public borderVisible = false;
   @Input() heroContent: any;
   constructor(
     @Inject(WINDOW) private window: Window, @Inject(DOCUMENT) private document: Document,
     private route: ActivatedRoute,
     private title: Title
-  ) {}
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.title.setTitle(this.heroContent);
-  }
-
-  showBorders(): boolean | void {
-    let isBorders;
     this.route.queryParams.subscribe((params) => {
-      if (params?.['showBorder'] == "true") {
-        isBorders = params?.['showBorder'];
+      if (params?.['showBorders'] == "true") {
+        this.borderVisible = (/true/i).test(params?.['showBorders']);
       }
     });
-    return isBorders;
+  }
+  showBorders(): string {
+    let style = '';
+    if (this.borderVisible) {
+      style = 'showBorders'
+    }
+    return style;
   }
 }
