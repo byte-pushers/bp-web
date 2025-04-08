dbce23b316388ae0a3e771ae59af56a6b23dbdd5
Surya <jsurya352@gmail.com>
2025-04-08 22:22:16 +0530

    feature/customComponents - created dialog service and started using it

--- bp-web-client/angular-app/src/app/shared/components/popup/popup.component.ts
+++ bp-web-client/angular-app/src/app/shared/components/popup/popup.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { DialogService } from '@app/services/dialog/dialog.service';
 import { CallToActionComponent } from '../call-to-action/call-to-action.component';
 
 @Component({
   selector: 'app-popup',
   standalone: true,
-  imports: [CallToActionComponent],
+  imports: [CallToActionComponent, NgIf],
   templateUrl: './popup.component.html',
   styleUrl: './popup.component.scss'
 })
-export class PopupComponent {
+export class PopupComponent implements OnInit {
+  isDialog: any;
+  constructor(private dialog: DialogService) { }
 
+  ngOnInit(): void {
+    this.dialog.openPopupObservable.subscribe(isActive => {
+      this.isDialog = isActive
+    })
+  }
 }
