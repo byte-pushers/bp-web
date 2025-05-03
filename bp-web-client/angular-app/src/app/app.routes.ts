7c0c7adc3314cb68e2c3353afaa44b3474afa2d4
Surya <jsurya352@gmail.com>
2025-05-03 15:35:54 +0530

    feature/customComponents - WIP adding more routes

--- bp-web-client/angular-app/src/app/app.routes.ts
+++ bp-web-client/angular-app/src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
-import { AppHomeComponent } from '@components/app-home/app-home.component';
+import { AppHomeComponent } from '@app/screens/app-home/app-home.component';
 
 export const routes: Routes = [
   { path: "", redirectTo: "/home", pathMatch: "full" },
-  { path: "home", component: AppHomeComponent }
+  { path: "home", component: AppHomeComponent },
+  { path: "home", component: AppHomeComponent },
+  { path: "home", component: AppHomeComponent },
+  { path: "home", component: AppHomeComponent },
+  { path: "home", component: AppHomeComponent },
+  { path: "home", component: AppHomeComponent },
 ];
