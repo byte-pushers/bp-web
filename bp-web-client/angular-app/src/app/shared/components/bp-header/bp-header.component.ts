c831a6783baf7abd33f64dc2c94b8bdc848247a3
Surya <jsurya352@gmail.com>
2025-05-17 10:10:59 +0530

    feature/customComponents - WIP header responsiveness

--- bp-web-client/angular-app/src/app/shared/components/bp-header/bp-header.component.ts
+++ bp-web-client/angular-app/src/app/shared/components/bp-header/bp-header.component.ts
@@ -1,24 +1,28 @@
 import { NgClass, NgIf } from '@angular/common';
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { LogoOnlyTextComponent } from '@app/components/logo-only-text/logo-only-text.component';
 import { LogoTextAsideComponent } from '@app/components/logo-text-aside/logo-text-aside.component';
 import { LogoTextBottomComponent } from '@app/components/logo-text-bottom/logo-text-bottom.component';
 import { BpButtonComponent } from '../bp-button/bp-button.component';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'bp-header',
   standalone: true,
-  imports: [NgClass, NgIf, LogoOnlyTextComponent, LogoTextAsideComponent, LogoTextBottomComponent, RouterLink, RouterLinkActive, BpButtonComponent],
+  imports: [NgClass, NgIf, FontAwesomeModule, LogoOnlyTextComponent, LogoTextAsideComponent, LogoTextBottomComponent, RouterLink, RouterLinkActive, BpButtonComponent],
   templateUrl: './bp-header.component.html',
   styleUrl: './bp-header.component.scss'
 })
-export class BpHeaderComponent {
+export class BpHeaderComponent implements OnInit {
+  faBars = faBars;
   isScrolled: boolean = false;
   screenWidth: any;
   constructor(private router: Router) {
 
   }
+  @HostListener('window:resize', ['$event'])
   @HostListener('window:scroll', ['$event'])
   onWindowScroll(event: Event): void {
     // Perform actions based on the scroll event
@@ -28,11 +32,21 @@ export class BpHeaderComponent {
       this.isScrolled = false;
     }
   }
-  @HostListener('window:resize', ['$event'])
+  ngOnInit(): void {
+    this.screenWidth = window.innerWidth;
+  }
   onResize(event: any) {
     this.screenWidth = window.innerWidth;
   }
+  setScreenWidth() {
+    this.screenWidth = window.innerWidth;
+  }
 
+
+  hideTill(till: any) {
+    this.setScreenWidth()
+    return this.screenWidth < till ? false : true;
+  }
   goHome() {
     this.router.navigate(['/home'])
   }
