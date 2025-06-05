71e8aafc07e71a2ea189e74b0525317ce27e3ee8
Surya <jsurya352@gmail.com>
2025-06-05 11:23:11 +0530

    feat/CTA-integration added ability to close the popup

--- bp-web-client/angular-app/src/app/shared/components/dialog/dialog.component.ts
+++ bp-web-client/angular-app/src/app/shared/components/dialog/dialog.component.ts
@@ -2,15 +2,17 @@ import { NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { DialogService } from '@app/services/dialog/dialog.service';
 import { CallToActionComponent } from '../call-to-action/call-to-action.component';
-
+import { faClose } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 @Component({
   selector: 'app-dialog',
   standalone: true,
-  imports: [CallToActionComponent, NgIf],
+  imports: [CallToActionComponent, NgIf, FontAwesomeModule],
   templateUrl: './dialog.component.html',
   styleUrl: './dialog.component.scss'
 })
 export class DialogComponent implements OnInit {
+  faClose = faClose;
   isDialog: any;
   constructor(private dialog: DialogService) { }
 
@@ -18,5 +20,8 @@ export class DialogComponent implements OnInit {
     this.dialog.openPopupObservable.subscribe(isActive => {
       this.isDialog = isActive
     })
+  }
+  closeDialog() {
+    this.dialog.hide()
   }
 }
