600ccbd76eb9fa94ab366bbb29d020bf239cf79b
Tonté Pouncil <tonte.pouncil@bytepushers.services>
2026-08-01 23:52:13 -0500

    feature/release-0.0.0.1-candidate-primary-cta(got working baseline changes related custom landing pages)

--- bp-web-client/angular-app/src/app/screens/home/home.component.ts
+++ bp-web-client/angular-app/src/app/screens/home/home.component.ts
@@ -90,8 +90,8 @@ export class HomeComponent implements OnInit, AfterViewInit {
 
   ngOnInit() {
     this.#setLandingPageId();
-    this.route?.queryParams.subscribe((params) => {
-      if (params?.['showBorders'] == "true") {
+    this.route.queryParams.subscribe((params) => {
+      if (params['showBorders'] == "true") {
         this.borderVisible = (/true/i).test(params?.['showBorders']);
       }
     });
@@ -120,9 +120,9 @@ export class HomeComponent implements OnInit, AfterViewInit {
   }
 
   #setLandingPageId() {
-    this.route?.queryParamMap?.subscribe((params) => {
-      this.#landingPageId = params?.get('id') ||  '';
-      this.#layoutType = params?.get('layout') || '';
+    this.route.queryParams.subscribe((params) => {
+      this.#landingPageId = params['id'] ||  '';
+      this.#layoutType = params['layout'] || '';
     });
   }
 
